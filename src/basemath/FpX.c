447dac3943e22b1e8870b323ac503855c6e6028d
Bill Allombert <Bill.Allombert@math.u-bordeaux1.fr>
2014-09-13 13:55:52 +0200

    FpXQ_autsum: use more straightforward formula.

--- src/basemath/FpX.c
+++ src/basemath/FpX.c
@@ -1504,13 +1504,14 @@ static GEN
 FpXQ_autsum_mul(void *E, GEN x, GEN y)
 {
   struct _FpXQ *D = (struct _FpXQ*)E;
+  GEN T = D->T, p = D->p;
   GEN phi1 = gel(x,1), a1 = gel(x,2);
   GEN phi2 = gel(y,1), a2 = gel(y,2);
   ulong d = brent_kung_optpow(maxss(degpol(phi1),degpol(a1)),2,1);
-  GEN V2 = FpXQ_powers(phi2,d,D->T,D->p);
-  GEN phi3 = FpX_FpXQV_eval(phi1,V2,D->T,D->p);
-  GEN aphi = FpX_FpXQV_eval(a1,V2,D->T,D->p);
-  GEN a3 = FpXQ_mul(aphi,a2,D->T,D->p);
+  GEN V1 = FpXQ_powers(phi1, d, T, p);
+  GEN phi3 = FpX_FpXQV_eval(phi2, V1, T, p);
+  GEN aphi = FpX_FpXQV_eval(a2, V1, T, p);
+  GEN a3 = FpXQ_mul(a1, aphi, T, p);
   return mkvec2(phi3, a3);
 }
 static GEN
