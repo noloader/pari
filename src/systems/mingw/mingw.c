f5d53f5005853395bf3c0dc33f9a04f5ab9fc6ca
Bill Allombert <Bill.Allombert@math.u-bordeaux1.fr>
2014-09-16 12:02:14 +0200

    win32_cb_alarm: fix warning about unused TimerOrWaitFired

--- src/systems/mingw/mingw.c
+++ src/systems/mingw/mingw.c
@@ -163,7 +163,7 @@ static HANDLE hTimerQueue = NULL;
 static void CALLBACK
 win32_cb_alarm(void *lpParam, BOOLEAN TimerOrWaitFired)
 {
-  (void) lpParam;
+  (void) lpParam; (void) TimerOrWaitFired;
   win32ctrlc++;
   win32alrm = 1;
 }
