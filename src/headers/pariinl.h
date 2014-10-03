deb8d304c213e2e5a22320b1d17711c8442bb03e
Bill Allombert <Bill.Allombert@math.u-bordeaux1.fr>
2014-10-03 18:49:51 +0200

    Add function FqX_powu

--- src/headers/pariinl.h
+++ src/headers/pariinl.h
@@ -2091,6 +2091,9 @@ INLINE GEN
 FqX_sqr(GEN x, GEN T, GEN p)
 { return T? FpXQX_sqr(x, T, p): FpX_sqr(x, p); }
 INLINE GEN
+FqX_powu(GEN x, ulong n, GEN T, GEN p)
+{ return T? FpXQX_powu(x, n, T, p): FpX_powu(x, n, p); }
+INLINE GEN
 FqX_div(GEN x, GEN y, GEN T, GEN p)
 { return T? FpXQX_divrem(x,y,T,p,NULL): FpX_divrem(x,y,p,NULL); }
 INLINE GEN
