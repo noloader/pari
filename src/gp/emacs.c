0435826ee5e6a065676afbf88c5e903178997e9f
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2014-10-08 08:18:23 +0200

    add missing #include "gp.h"

--- src/gp/emacs.c
+++ src/gp/emacs.c
@@ -18,6 +18,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
 /*******************************************************************/
 #include "pari.h"
 #include "paripriv.h"
+#include "gp.h"
 void
 init_emacs(void)
 {
