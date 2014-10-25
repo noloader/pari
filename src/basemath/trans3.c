noloader/pari

commit 7b7e4425b894e43ded2788735099c19814015611

nbits2prec, nbits2extraprec: do not call with negative argument

src/basemath/trans3.c
@@ -659,7 +659,7 @@ incgam_0(GEN x, GEN expx)
   }
   else
   {
-    long prec = l + (long)nbits2extraprec((mx+log(mx))/LOG2 + 10);
+    long prec = nbits2prec(prec2nbits(l) + (mx+log(mx))/LOG2 + 10);
     GEN S, t, H, run = real_1(prec);
     n = -prec2nbits(prec);
     x = rtor(x, prec);
@@ -1302,11 +1302,11 @@ inv_szeta_euler(long n, double lba, long prec)
   av2 = avma; A = n / LOG2;
   while ((p = u_forprime_next(&S)))
   {
-    long l = prec - nbits2extraprec((long)floor(A * log(p)) - BITS_IN_LONG);
+    long l = prec2nbits(prec) - (long)floor(A * log(p)) - BITS_IN_LONG;
     GEN h;
 
-    if (l < 3)         l = 3;
-    else if (l > prec) l = prec;
+    if (l < BITS_IN_LONG) l = BITS_IN_LONG;
+    l = maxss(prec, nbits2prec(l));
     h = divrr(z, rpowuu(p, (ulong)n, l));
     z = subrr(z, h);
     if (gc_needed(av,1))

src/basemath/Qfb.c
@@ -953,9 +953,9 @@ static GEN
 qfr5_init(GEN x, struct qfr_data *S)
 {
   GEN d = gel(x,4);
-  long prec = realprec(d), l = nbits2prec(-expo(d));
-  if (l > prec) prec = l;
-  if (prec < LOWDEFAULTPREC) prec = LOWDEFAULTPREC;
+  long prec = realprec(d), l = -expo(d);
+  if (l < BITS_IN_LONG) l = BITS_IN_LONG;
+  prec = maxss(prec, nbits2prec(l));
   x = qfr_to_qfr5(x,prec);
 
   get_disc(x, S);

src/modules/stark.c
@@ -19,8 +19,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
 #include "pari.h"
 #include "paripriv.h"
 
-#define EXTRA_PREC (DEFAULTPREC-2)
-#define ADD_PREC   (DEFAULTPREC-2)*3
+#define EXTRA_PREC DEFAULTPREC-2
 
 /* ComputeCoeff */
 typedef struct {
@@ -2583,9 +2582,9 @@ LABDOUB:
           a) get at least EXTRA_PREC fractional digits if there is none;
        or b) double the fractional digits.
     */
-    incr_pr = nbits2extraprec( prec2nbits(gprecision(polrelnum))- gexpo(polrelnum) );
+    incr_pr = prec2nbits(gprecision(polrelnum))- gexpo(polrelnum);
     if (incr_pr < 0) incr_pr = -incr_pr + EXTRA_PREC;
-    newprec = newprec + maxss(ADD_PREC, cpt*incr_pr);
+    newprec += nbits2extraprec(maxss(3*EXTRA_PREC, cpt*incr_pr));
     if (DEBUGLEVEL) pari_warn(warnprec, "AllStark", newprec);
 
     nf = nfnewprec_shallow(nf, newprec);
@@ -3234,7 +3233,7 @@ quadhilbertimag(GEN D)
     P = grndtoi(P,&exmax);
     if (DEBUGLEVEL>1) timer_printf(&ti,"product, error bits = %ld",exmax);
     if (exmax <= -10) break;
-    avma = av0; prec += (DEFAULTPREC-2) + nbits2extraprec(exmax);
+    avma = av0; prec += nbits2extraprec(prec2nbits(DEFAULTPREC)+exmax);
     if (DEBUGLEVEL) pari_warn(warnprec,"quadhilbertimag",prec);
   }
   return gerepileupto(av,P);

commit 43e15cbef0987344c8de5ff5be3f3dee1a2791a0

fix typo / regression from commit 7b7e4425b
bernfrac(10000): x5 slowdown

src/basemath/trans3.c
@@ -1306,7 +1306,7 @@ inv_szeta_euler(long n, double lba, long prec)
     GEN h;
 
     if (l < BITS_IN_LONG) l = BITS_IN_LONG;
-    l = maxss(prec, nbits2prec(l));
+    l = minss(prec, nbits2prec(l));
     h = divrr(z, rpowuu(p, (ulong)n, l));
     z = subrr(z, h);
     if (gc_needed(av,1))