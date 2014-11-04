noloader/pari

commit 76f168eb840e7a5dd1ce91183d094b737316a4f1

68- possibly missing roots in nfroots (when using Trager)

src/basemath/nffactor.c
@@ -1701,6 +1701,7 @@ nfsqff_trager(GEN u, GEN T, GEN dent)
       pari_err_IRREDPOL("factornf [modulus]",T);
     gel(P,i) = QXQX_normalize(F, T);
   }
+  gen_sort_inplace(P, (void*)&cmp_RgX, cmp_nodata, NULL);
   return P;
 }
 

commit 73328690b1b52565d9809275dd1ef682d3a9088c

fix typo introduced in previous commit 76f168eb

src/basemath/nffactor.c
@@ -1701,7 +1701,7 @@ nfsqff_trager(GEN u, GEN T, GEN dent)
       pari_err_IRREDPOL("factornf [modulus]",T);
     gel(P,i) = QXQX_normalize(F, T);
   }
-  gen_sort_inplace(P, (void*)&cmp_RgX, cmp_nodata, NULL);
+  gen_sort_inplace(P, (void*)&cmp_RgX, (void*)&gen_cmp_RgX, NULL);
   return P;
 }
 