noloader/pari

commit 271d7b1af0cdce2254b5337cc1e9860653fc5310

introduce varpriority + fix MAXVARN (ab)uses
- varpriority array initialized to priority[i] = -i: allow to use
  pol_x(i) directly.

- fix originally broken quadray

- remove all usage of MAXVARN as a variable number

- add GP function varhigher()

- write "temporary variables" as t<num> rather than #<num>
  [ allow copy-paste ]

- cleanups in entree structs allocation / handling, e.g. remove kludges
  related to findentry.

  FIXME: later switch to plain hashtables
  FIXME: use a different hashtable for member functions and standard symbols.

- introduce full hash in entree structure

- OCstoredyn: don't create variable, don't call checkvalue. Reduces
  drastically the number of created polynomial variables.

  FIXME: do we want to delete an EpINSTALL via f = <newval> ? No for the
  time being.

  FIXME: MAXVARN should now be reduced; e.g. 2^10 polynomial variables
  should be more than enough and will allow to detect leaks sooner.

- don't delete_var() on pari_close

src/basemath/gen3.c
@@ -362,9 +362,8 @@ degree(GEN x)
 GEN
 pollead(GEN x, long v)
 {
-  long l, tx = typ(x), w;
+  long tx = typ(x), w;
   pari_sp av;
-  GEN xinit;
 
   if (is_scalar_t(tx)) return gcopy(x);
   w = varn(x);
@@ -373,33 +372,25 @@ pollead(GEN x, long v)
     case t_POL:
       if (v < 0 || v == w)
       {
-        l=lg(x);
+        long l = lg(x);
         return (l==2)? gen_0: gcopy(gel(x,l-1));
       }
       break;
 
     case t_SER:
       if (v < 0 || v == w) return signe(x)? gcopy(gel(x,2)): gen_0;
+      if (varncmp(v, w) > 0) x = polcoeff_i(x, valp(x), v);
       break;
 
     default:
       pari_err_TYPE("pollead",x);
       return NULL; /* not reached */
   }
-  if (v < w) return gcopy(x);
-  av = avma; xinit = x;
-  x = gsubst(gsubst(x,w,pol_x(MAXVARN)),v,pol_x(0));
-  if (gvar(x)) { avma = av; return gcopy(xinit);}
-  tx = typ(x);
-  if (tx == t_POL) {
-    l = lg(x); if (l == 2) { avma = av; return gen_0; }
-    x = gel(x,l-1);
-  }
-  else if (tx == t_SER) {
-    if (!signe(x)) { avma = av; return gen_0;}
-    x = gel(x,2);
-  } else pari_err_TYPE("pollead",x);
-  return gerepileupto(av, gsubst(x,MAXVARN,pol_x(w)));
+  if (varncmp(v, w) < 0) return gcopy(x);
+  av = avma;
+  x = gsubst(x, v, pol_x(fetch_var_higher()));
+  x = pollead(x, -1);
+  delete_var(); return gerepileupto(av, x);
 }
 
 /* returns 1 if there's a real component in the structure, 0 otherwise */
@@ -1603,7 +1594,7 @@ derivser(GEN x)
 GEN
 deriv(GEN x, long v)
 {
-  long lx, vx, tx, i, j;
+  long lx, tx, i, j;
   pari_sp av;
   GEN y;
 
@@ -1625,17 +1616,21 @@ deriv(GEN x, long v)
       retmkpolmod(deriv(a,v), RgX_copy(b));
     }
     case t_POL:
-      vx = varn(x);
-      if (varncmp(vx, v) > 0) return RgX_get_0(x);
-      if (varncmp(vx, v) == 0) return RgX_deriv(x);
+      switch(varncmp(varn(x), v))
+      {
+        case 1: return RgX_get_0(x);
+        case 0: return RgX_deriv(x);
+      }
       y = cgetg_copy(x, &lx); y[1] = x[1];
       for (i=2; i<lx; i++) gel(y,i) = deriv(gel(x,i),v);
       return normalizepol_lg(y,i);
 
     case t_SER:
-      vx = varn(x);
-      if (varncmp(vx, v) > 0) return RgX_get_0(x);
-      if (varncmp(vx, v) == 0) return derivser(x);
+      switch(varncmp(varn(x), v))
+      {
+        case 1: return RgX_get_0(x);
+        case 0: return derivser(x);
+      }
       if (ser_isexactzero(x)) return gcopy(x);
       y = cgetg_copy(x, &lx); y[1] = x[1];
       for (j=2; j<lx; j++) gel(y,j) = deriv(gel(x,j),v);
@@ -1775,20 +1770,10 @@ diffop0(GEN x, GEN v, GEN dv, long n)
 static GEN
 swapvar_act(GEN x, long vx, long v, GEN (*act)(void*, long, GEN), void *data)
 {
-  GEN y;
-  if (v != MAXVARN) { /* (vx,v) -> (MAXVARN, v) */
-    y = act(data, v, gsubst(x,vx,pol_x(MAXVARN)));
-    y = gsubst(y,MAXVARN,pol_x(vx));
-  } else if (vx != 0) { /* (vx,v) -> (vx, 0) */
-    y = act(data, 0, gsubst(x,v,pol_x(0)));
-    y = gsubst(y,0,pol_x(v));
-  } else { /* (0,MAXVARN) -> (w, 0) */
-    long w = fetch_var();
-    y = act(data, 0, gsubst(gsubst(x,0,pol_x(w)), MAXVARN,pol_x(0)));
-    y = gsubst(gsubst(y,0,pol_x(MAXVARN)), w,pol_x(0));
-    (void)delete_var();
-  }
-  return y;
+  long v0 = fetch_var();
+  GEN y = act(data, v, gsubst(x,vx,pol_x(v0)));
+  y = gsubst(y,v0,pol_x(vx));
+  (void)delete_var(); return y;
 }
 /* x + O(v^data) */
 static GEN

src/test/32/pol
@@ -18,7 +18,7 @@ y^2 + 2*y - 4 x^2 + 2*x - 4 -4*x^2 + 2*x + 1
 y + 2*y^2 + O(y^4) (y + 2*y^2 + O(y^4)) + O(x^16) (y + 2*y^2 + O(y^4)) + O(x
 ^5)
 2 2 3
-y + 2 y + 2 (y + 2) + x + O(x^2)
+y + 2 y + 2 1
 1 1 x + 2
 x^2 + x + 1
 -x^3 - 2*x^2 - x + 1

COMPAT
@@ -20,6 +20,12 @@ This file lists the incompatible changes between Version 2.x and older versions
   - ellbil(E,P,Q) is now deprecated, use ellheight(E,P,Q)
   - default 'strictmatch' has been obsoleted. It is now a no-op.
   - default 'compatible' has been obsoleted. It is now a no-op.
+  - GP and libpari polynomial variables of arbitrary priority can now be
+    created: 'x' is no longer guaranteed to have maximal priority,
+    nor MAXVARN to have minimal priority.
+  - GP: polynomial variables no longer spring into existence unless a true
+    t_POL/t_SER in the variable is needed. E.g. y = 0 does not create the
+    "variable" y, thereby messing up variable ordering.
 
 * Specific to the PARI library:
 ===============================

doc/usersch2.tex
@@ -1297,7 +1297,9 @@ The answer is that variables are ordered implicitly by the interpreter:
 when a new identifier (e.g~$x$, or $y$ as above) is input, the corresponding
 variable is registered as having a strictly lower priority than any variable in
 use at this point\footnote{*}{This is not strictly true:
-the variable $x$ is predefined and always has the highest possible priority.}%
+the variable $x$ is predefined and initially has the highest possible
+priority. Variables of higher priority than $x$ can be created
+using \tet{varhigher}.}%
 . To see the ordering used by \kbd{gp} at any given time, type
 \kbd{variable()}.\kbdsidx{variable}
 

doc/usersch4.tex
@@ -1573,14 +1573,7 @@ situation.
 Some initializations are needed before you can use a given integer $v$ as a
 variable number.
 
-Initially, this is done for $0$ (the variable \kbd{x} under \kbd{gp}), and
-\tet{MAXVARN}, which is there to address the need for a ``temporary'' new
-variable in library mode and cannot be input under \kbd{gp}. No documented
-library function can create from scratch an object involving \tet{MAXVARN}
-(of course, if the operands originally involve \kbd{MAXVARN}, the function
-abides). We call the latter type a ``temporary variable''. The regular
-variables meant to be used in regular objects, are called ``user
-variables\sidx{variable (user)}''.
+Initially, this is done for $0$ (the variable \kbd{x} under \kbd{gp}).
 
 \subsubsec{User variables}\sidx{variable (user)} When the program starts,
 \kbd{x} is the only user variable (number~$0$). To define new ones, use
@@ -1618,24 +1611,22 @@ that this may not be the same as \kbd{pol\_x(v)} if assignments have been
 performed in the interpreter.
 
 \subsubsec{Temporary variables}\sidx{variable (temporary)}
-\kbd{MAXVARN} is available, but is better left to PARI internal functions
-(some of which do not check that \kbd{MAXVARN} is free for them to use,
-which can be considered a bug). You can create more temporary variables
-using
+You can create temporary variables using
 
 \fun{long}{fetch_var}{}\label{se:fetch_var}
+returns a new variable with \emph{lower} priority than any variable currently
+in use.
+
+\fun{long}{fetch_var_higher}{}\label{se:fetch_var}
+returns a new variable with \emph{higher} priority than any variable
+currently in use.
 
 \noindent
-This returns a variable number which is guaranteed to be unused by the
-library at the time you get it and as long as you do not delete it (we will
-see how to do that shortly). This has \emph{higher} priority than any
-temporary variable produced so far (\kbd{MAXVARN} is assumed to be the first
-such). After the statement \kbd{v = fetch\_var()}, you can use
+After the statement \kbd{v = fetch\_var()}, you can use
 \kbd{pol\_1(v)} and \kbd{pol\_x(v)}. The variables created in this way have no
 identifier assigned to them though, and are printed as
-\kbd{\#<\text{number}>}, except for \kbd{MAXVARN} which is printed
-as~\kbd{\#}. You can assign a name to a temporary variable, after creating
-it, by calling the function
+\kbd{t\text{number}}. You can assign a name to a temporary variable, after
+creating it, by calling the function
 
 \fun{void}{name_var}{long n, char *s}
 
@@ -1643,8 +1634,9 @@ it, by calling the function
 represent the variable number~\kbd{n}. The GP parser will \emph{not}
 recognize it by that name, however, and calling this on a variable known
 to~\kbd{gp} raises an error. Temporary variables are meant to be used as free
-variables, and you should never assign values or functions to them as you
-would do with variables under~\kbd{gp}. For that, you need a user variable.
+variables to build polynomials and power series, and you should never assign
+values or functions to them as you would do with variables under~\kbd{gp}.
+For that, you need a user variable.
 
 All objects created by \kbd{fetch\_var} are on the heap and not on the stack,
 thus they are not subject to standard garbage collecting (they are not
@@ -1654,8 +1646,8 @@ not need a variable number anymore, you can delete it using
 \fun{long}{delete_var}{}
 
 \noindent which deletes the \emph{latest} temporary variable created and
-returns the variable number of the previous one (or simply returns 0 if you
-try, in vain, to delete \kbd{MAXVARN}). Of course you should make sure that
+returns the variable number of the previous one (or simply returns 0 if none
+remain). Of course you should make sure that
 the deleted variable does not appear anywhere in the objects you use later
 on. Here is an example:
 
@@ -1672,8 +1664,7 @@ The (dangerous) statement
 \bprog
   while (delete_var()) /*@Ccom empty */;
 @eprog\noindent
-removes all temporary variables in use, except \kbd{MAXVARN} which cannot be
-deleted.
+removes all temporary variables in use.
 
 \section{Input and output}
 

doc/usersch5.tex
@@ -1316,19 +1316,14 @@ variable we can create.
 \fun{long}{pari_var_next_temp}{} returns \kbd{max\_avail}, the number of the
 next temp variable we can create.
 
-\fun{void}{pari_var_create}{entree *ep} low-level initialization of an
-\kbd{EpVAR}.
+\fun{long}{pari_var_create}{entree *ep} low-level initialization of an
+\kbd{EpVAR}. Return the associated (new) variable number.
 
 \subsec{User variables}
 
 \fun{long}{fetch_user_var}{char *s} returns a user variable whose name
 is \kbd{s}, creating it is needed (and using an existing variable otherwise).
 Returns its variable number.
-
-\fun{entree*}{fetch_named_var}{char *s} as \kbd{fetch\_user\_var}, but
-returns an \kbd{entree*} suitable for inclusion in the interpreter hashlists
-of symbols, not a variable number. \kbd{fetch\_user\_var} is a trivial
-wrapper.
 
 \fun{GEN}{fetch_var_value}{long v} returns a shallow copy of the
 current value of the variable numbered $v$. Return \kbd{NULL} for a temporary
@@ -11420,8 +11415,9 @@ instruction request the iterator to terminate immediately.
 
 \section{Defaults}
 
-\fun{int}{pari_is_default}{const char *s} return $1$ if $s$ is the name
-of a default, $0$ otherwise.
+\fun{entree*}{pari_is_default}{const char *s} return the \kbd{entree}
+structure associated to $s$ if it is the name of a default, \kbd{NULL}
+otherwise.
 
 \fun{GEN}{setdefault}{const char *s, const char *v, long flag} is the
 low-level function underlying \kbd{default0}. If $s$ is \kbd{NULL}, call all

src/basemath/Flx.c
@@ -2711,8 +2711,9 @@ Flxq_charpoly(GEN x, GEN TB, ulong p)
 {
   pari_sp ltop=avma;
   GEN T = get_Flx_mod(TB);
-  GEN xm1 = deg1pol_shallow(pol1_Flx(x[1]),Flx_neg(x,p),evalvarn(MAXVARN));
-  return gerepileupto(ltop, Flx_FlxY_resultant(T, xm1 ,p));
+  GEN xm1 = deg1pol_shallow(pol1_Flx(x[1]),Flx_neg(x,p),evalvarn(fetch_var()));
+  GEN r = Flx_FlxY_resultant(T, xm1, p);
+  (void)delete_var(); return gerepileupto(ltop, r);
 }
 
 GEN

src/basemath/base1.c
@@ -1176,6 +1176,7 @@ nfiso0(GEN a, GEN b, long fliso)
   pari_sp av = avma;
   long i, vb, lx;
   GEN nfa, nfb, y, la, lb;
+  int newvar;
 
   a = get_nfpol(a, &nfa);
   b = get_nfpol(b, &nfb);
@@ -1186,16 +1187,12 @@ nfiso0(GEN a, GEN b, long fliso)
 
   if (nfb) lb = gen_1; else b = ZX_Q_normalize(b,&lb);
   if (nfa) la = gen_1; else a = ZX_Q_normalize(a,&la);
-  a = leafcopy(a); setvarn(a,0);
-  b = leafcopy(b); vb = varn(b);
+  vb = varn(b); newvar = (varncmp(vb,varn(a)) <= 0);
+  if (newvar) { a = leafcopy(a); setvarn(a, fetch_var_higher()); }
   if (nfb)
-  {
-    if (vb == 0) nfb = gsubst(nfb, 0, pol_x(MAXVARN));
     y = lift_intern(nfroots(nfb,a));
-  }
   else
   {
-    if (vb == 0) setvarn(b, fetch_var());
     y = gel(polfnf(a,b),1); lx = lg(y);
     for (i=1; i<lx; i++)
     {
@@ -1203,10 +1200,10 @@ nfiso0(GEN a, GEN b, long fliso)
       if (degpol(t) != 1) { setlg(y,i); break; }
       gel(y,i) = gneg_i(lift_intern(gel(t,2)));
     }
-    if (vb == 0) (void)delete_var();
     settyp(y, t_VEC);
     gen_sort_inplace(y, (void*)&cmp_RgX, &cmp_nodata, NULL);
   }
+  if (newvar) (void)delete_var();
   lx = lg(y); if (lx==1) { avma=av; return gen_0; }
   for (i=1; i<lx; i++)
   {

commit 088b8aeceb3882efa74fe292ede031e3a0299c99

fix pollead problem introduced in 271d7b1a
pollead(z,y)
pollead(y,z)

src/basemath/gen3.c
@@ -387,9 +387,9 @@ pollead(GEN x, long v)
       return NULL; /* not reached */
   }
   if (varncmp(v, w) < 0) return gcopy(x);
-  av = avma;
-  x = gsubst(x, v, pol_x(fetch_var_higher()));
-  x = pollead(x, -1);
+  av = avma; w = fetch_var_higher();
+  x = gsubst(x, v, pol_x(w));
+  x = pollead(x, w);
   delete_var(); return gerepileupto(av, x);
 }
 

src/test/32/pol
@@ -18,8 +18,10 @@ y^2 + 2*y - 4 x^2 + 2*x - 4 -4*x^2 + 2*x + 1
 y + 2*y^2 + O(y^4) (y + 2*y^2 + O(y^4)) + O(x^16) (y + 2*y^2 + O(y^4)) + O(x
 ^5)
 2 2 3
-y + 2 y + 2 1
+y + 2 y + 2 x + 2
 1 1 x + 2
+z
+y
 x^2 + x + 1
 -x^3 - 2*x^2 - x + 1
 [4, 0, 0, 0, -2]~

src/test/in/pol
@@ -13,6 +13,8 @@ o = [2*x+3*y, 2+x+y+O(x^2), 2+x+y+O(y^2)];
     printsep(" ",pollead(v), pollead(v,x), pollead(v,y))
   )
 }
+pollead(z,y)
+pollead(y,z)
 polgraeffe(x^2+x+1)
 polgraeffe(x^3+x+1)
 polsym(2*x^4+1,4)