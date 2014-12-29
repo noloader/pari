noloader/pari

commit 58f7433c99d1feeae60588d7157432c25e00b0d0

47- [libpari] rename 'define' PI -> M_PI and use proper constant

src/basemath/trans1.c
@@ -127,7 +127,7 @@ abpq_sum(struct abpq_res *r, long n1, long n2, struct abpq *A)
 
 /********************************************************************/
 /**                                                                **/
-/**                               PI                               **/
+/**                               M_PI                               **/
 /**                                                                **/
 /********************************************************************/
 /* replace *old clone by c. Protect against SIGINT */
@@ -2679,7 +2679,7 @@ mpcosm1(GEN x, long *ptmod8)
       q = floorr( divrr(z,pitemp) ); /* round ( x / (Pi/2) ) */
       p = l+EXTRAPRECWORD; x = rtor(x,p);
     } else {
-      q = stoi((long)floor(rtodbl(x) / (PI/2) + 0.5));
+      q = stoi((long)floor(rtodbl(x) / (M_PI/2) + 0.5));
       p = l;
     }
     if (signe(q))

COMPAT
@@ -46,6 +46,7 @@ This file lists the incompatible changes between Version 2.x and older versions
   - comment out libpari function names obsoleted during the 2.3.* cycle
     See PARI_OLD_NAMES.
   - ellsea is replaced by Fp_ellcard_SEA
+  - rename PI -> M_PI
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%% VERSION 2.6 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   - eval() no longer evaluates its arguments in "permissive" mode: eval("1a")

doc/usersch5.tex
@@ -839,7 +839,7 @@ These are \kbd{double} approximations to useful constants:
 
 \noindent \tet{LOG2_10}: $\log 10 / \log 2$.
 
-\noindent \tet{PI}: $\pi$.
+\noindent \tet{M_PI}: $\pi$.
 
 \section{Iterating over small primes, low-level interface}
 \label{se:primetable}

src/basemath/buch1.c
@@ -1048,7 +1048,7 @@ START:
   }
   while (nsubFB < (expi(D) > 15 ? 3 : 2));
   /* invhr = 2^r1 (2pi)^r2 / sqrt(D) w ~ L(chi,1) / hR */
-  invhr = gmul(dbltor((BQ.PRECREG?2.:PI)/sdrc), compute_invresquad(&GRHcheck));
+  invhr = gmul(dbltor((BQ.PRECREG?2.:M_PI)/sdrc), compute_invresquad(&GRHcheck));
   BQ.powsubFB = powsubFBquad(&BQ,CBUCH+1);
   if (DEBUGLEVEL>2) timer_printf(&T, "powsubFBquad");
   BQ.limhash = (LIMC & HIGHMASK)? (HIGHBIT>>1): LIMC*LIMC;

src/basemath/buch2.c
@@ -434,12 +434,12 @@ pre_allocate(RELCACHE_t *cache, size_t n)
 void
 init_GRHcheck(GRHcheck_t *S, long N, long R1, double LOGD)
 {
-  const double c1 = PI*PI/2;
+  const double c1 = M_PI*M_PI/2;
   const double c2 = 3.663862376709;
   const double c3 = 3.801387092431; /* Euler + log(8*Pi)*/
   S->clone = 0;
   S->cN = R1*c2 + N*c1;
-  S->cD = LOGD - N*c3 - R1*PI/2;
+  S->cD = LOGD - N*c3 - R1*M_PI/2;
   S->maxprimes = 16000; /* sufficient for LIMC=176081*/
   S->primes = (GRHprime_t*)pari_malloc(S->maxprimes*sizeof(*S->primes));
   S->nprimes = 0;
@@ -3857,7 +3857,7 @@ Buchall_param(GEN P, double cbach, double cbach2, long nbrelpid, long flun, long
   D = absi(nf_get_disc(nf)); drc = gtodouble(D);
   if (DEBUGLEVEL) err_printf("R1 = %ld, R2 = %ld\nD = %Ps\n",R1,R2, D);
   LOGD = log(drc); LOGD2 = LOGD*LOGD;
-  lim = exp(-N + R2 * log(4/PI)) * sqrt(2*PI*N*drc);
+  lim = exp(-N + R2 * log(4/M_PI)) * sqrt(2*M_PI*N*drc);
   if (lim < 3.) lim = 3.;
   if (cbach > 12.) {
     if (cbach2 < cbach) cbach2 = cbach;

src/basemath/ellanal.c
@@ -179,12 +179,12 @@ param_points(GEN N, double Y, double tmax, long bprec, long *cprec, long *L,
   double D, a, aY, X, logM;
   long d = 2, w = 1;
   tmax *= d;
-  D = bprec * LOG2 + PI/4*tmax + 2;
+  D = bprec * LOG2 + M_PI/4*tmax + 2;
   *cprec = nbits2prec(ceil(D / LOG2) + 5);
-  a = 2 * PI / sqrt(gtodouble(N));
-  aY = a * cos(PI/2*Y);
-  logM = log(4) + logboundG0(w+1, aY) + tmax * Y * PI/2;
-  *h = ( 2 * PI * PI / 2 * Y ) / ( D + logM );
+  a = 2 * M_PI / sqrt(gtodouble(N));
+  aY = a * cos(M_PI/2*Y);
+  logM = log(4) + logboundG0(w+1, aY) + tmax * Y * M_PI/2;
+  *h = ( 2 * M_PI * M_PI / 2 * Y ) / ( D + logM );
   X = log( D / a);
   *L = ceil( X / *h);
   *K = ceil_safe(dbltor( D / a ));

src/basemath/elliptic.c
@@ -2955,7 +2955,7 @@ ellperiods(GEN w, long flag, long prec)
 
 /* 2Pi Im(z)/log(2) */
 static double
-get_toadd(GEN z) { return (2*PI/LOG2)*gtodouble(imag_i(z)); }
+get_toadd(GEN z) { return (2*M_PI/LOG2)*gtodouble(imag_i(z)); }
 
 /* computes the numerical value of wp(z | L), L = om1 Z + om2 Z
  * return NULL if z in L.  If flall=1, compute also wp' */

src/basemath/trans2.c
@@ -82,7 +82,7 @@ mpatan(GEN x)
   if (e < -100)
     alpha = 1.65149612947 - e; /* log_2(Pi) - e */
   else
-    alpha = log2(PI / atan(rtodbl(p1)));
+    alpha = log2(M_PI / atan(rtodbl(p1)));
   beta = (double)(prec2nbits(l)>>1);
   delta = 1 + beta - alpha/2;
   if (delta <= 0) { n = 1; m = 0; }
@@ -971,11 +971,11 @@ double
 darg(double s, double t)
 {
   double x;
-  if (!t) return (s>0)? 0.: PI;
-  if (!s) return (t>0)? PI/2: -PI/2;
+  if (!t) return (s>0)? 0.: M_PI;
+  if (!s) return (t>0)? M_PI/2: -M_PI/2;
   x = atan(t/s);
   return (s>0)? x
-              : ((t>0)? x+PI : x-PI);
+              : ((t>0)? x+M_PI : x-M_PI);
 }
 
 void
@@ -1037,7 +1037,7 @@ cxgamma(GEN s0, int dolog, long prec)
     iS = imag_i(S);
     if (et > 0 && l > 0)
     {
-      GEN t = gmul(iS, dbltor(PI / l)), logt = glog(t,LOWDEFAULTPREC);
+      GEN t = gmul(iS, dbltor(M_PI / l)), logt = glog(t,LOWDEFAULTPREC);
       la = gmul(t, logt);
       if      (gcmpgs(la, 3) < 0)   { logla = log(3.); la = stoi(3); }
       else if (gcmpgs(la, 150) > 0) { logla = rtodbl(logt); la = t; }
@@ -1050,7 +1050,7 @@ cxgamma(GEN s0, int dolog, long prec)
     lim = (long)ceil(l / (1.+ logla));
     if (lim == 0) lim = 1;
 
-    u = gmul(la, dbltor((lim-0.5)/PI));
+    u = gmul(la, dbltor((lim-0.5)/M_PI));
     l2 = gsub(gsqr(u), gsqr(iS));
     if (signe(l2) > 0)
     {
@@ -1082,7 +1082,7 @@ cxgamma(GEN s0, int dolog, long prec)
     /* Im (s - 1/2) log(s) */
     v = (ssig - 0.5)*ilogs + st * rlogs;
     /* l2 = | (s - 1/2) log(s) - s + log(2Pi)/2 |^2 ~ |lngamma(s))|^2 */
-    u = u - ssig + log(2.*PI)/2;
+    u = u - ssig + log(2.*M_PI)/2;
     v = v - st;
     l2 = u*u + v*v;
     if (l2 < 0.000001) l2 = 0.000001;
@@ -1092,7 +1092,7 @@ cxgamma(GEN s0, int dolog, long prec)
     la = 3.; /* FIXME: heuristic... */
     if (st > 1 && l > 0)
     {
-      double t = st * PI / l;
+      double t = st * M_PI / l;
       la = t * log(t);
       if (la < 3) la = 3.;
       if (la > 150) la = t;
@@ -1100,7 +1100,7 @@ cxgamma(GEN s0, int dolog, long prec)
     lim = (long)ceil(l / (1.+ log(la)));
     if (lim == 0) lim = 1;
 
-    u = (lim-0.5) * la / PI;
+    u = (lim-0.5) * la / M_PI;
     l2 = u*u - st*st;
     if (l2 > 0)
     {
@@ -1556,7 +1556,7 @@ cxpsi(GEN s0, long prec)
     lim = 2 + (long)ceil((prec2nbits_mul(prec, LOG2) - l) / (2*(1+log((double)la))));
     if (lim < 2) lim = 2;
 
-    l = (2*lim-1)*la / (2.*PI);
+    l = (2*lim-1)*la / (2.*M_PI);
     L = gsub(dbltor(l*l), gsqr(iS));
     if (signe(L) < 0) L = gen_0;
 
@@ -1579,7 +1579,7 @@ cxpsi(GEN s0, long prec)
     lim = 2 + (long)ceil((prec2nbits_mul(prec, LOG2) - l) / (2*(1+log((double)la))));
     if (lim < 2) lim = 2;
 
-    l = (2*lim-1)*la / (2.*PI);
+    l = (2*lim-1)*la / (2.*M_PI);
     l = l*l - st*st;
     if (l < 0.) l = 0.;
     nn = (long)ceil( sqrt(l) - ssig );

commit 7ea4f465057b7c084c8263d58d41d01ff635e2fc

Fix comment
commit 58f7433c99d1feeae60588d7157432c25e00b0d0
mistakenly changed a comment

src/basemath/trans1.c
@@ -127,7 +127,7 @@ abpq_sum(struct abpq_res *r, long n1, long n2, struct abpq *A)
 
 /********************************************************************/
 /**                                                                **/
-/**                               M_PI                               **/
+/**                               PI                               **/
 /**                                                                **/
 /********************************************************************/
 /* replace *old clone by c. Protect against SIGINT */