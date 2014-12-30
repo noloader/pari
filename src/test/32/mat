noloader/pari

commit c4e44b673ad83f878714fccc841eaa54d3a17ed1

44- matkerint: replace underlying LLL algorithm by mathnf
Simple bench: M=matrix(50,55,i,j,random(10^5)); \\ 200 times faster

src/basemath/lll.c
@@ -794,32 +794,32 @@ qflllgram0(GEN x, long flag)
 /**                   INTEGRAL KERNEL (LLL REDUCED)                **/
 /**                                                                **/
 /********************************************************************/
-/* Horribly slow (coeff explosion in small dimension): never use this */
 static GEN
-kerint1(GEN x)
+kerint0(GEN M)
+{
+  GEN U, H = ZM_hnfall(M,&U,1);
+  long d = lg(M)-lg(H);
+  if (d == 1) return cgetg(1, t_MAT);
+  return ZM_lll(vecslice(U,1,d), LLLDFT, LLL_INPLACE);
+}
+GEN
+kerint(GEN M)
 {
   pari_sp av = avma;
-  return gerepilecopy(av, ZM_lll(QM_ImQ_hnf(ker(x)), LLLDFT, LLL_INPLACE));
+  return gerepilecopy(av, kerint0(M));
 }
-/* Mostly useless: use ZM_lll(x, 0.99, LLL_KER) directly */
+/* OBSOLETE: use kerint */
 GEN
-kerint(GEN x)
+matkerint0(GEN M, long flag)
 {
   pari_sp av = avma;
-  GEN h = ZM_lll(x, LLLDFT, LLL_KER);
-  if (lg(h)==1) { avma = av; return cgetg(1, t_MAT); }
-  return gerepilecopy(av, ZM_lll(h, LLLDFT, LLL_INPLACE));
-}
-
-GEN
-matkerint0(GEN x, long flag)
-{
-  if (typ(x) != t_MAT) pari_err_TYPE("matkerint",x);
-  RgM_check_ZM(x, "kerint");
+  if (typ(M) != t_MAT) pari_err_TYPE("matkerint",M);
+  M = Q_primpart(M);
+  RgM_check_ZM(M, "kerint");
   switch(flag)
   {
-    case 0: return kerint(x);
-    case 1: return kerint1(x);
+    case 0:
+    case 1: return gerepilecopy(av, kerint0(M));
     default: pari_err_FLAG("matkerint");
   }
   return NULL; /* not reached */

src/functions/linear_algebra/matkerint
@@ -3,20 +3,12 @@ Section: linear_algebra
 C-Name: matkerint0
 Prototype: GD0,L,
 Help: matkerint(x,{flag=0}): LLL-reduced Z-basis of the kernel of the matrix
- x with integral entries. flag is optional, and may be set to 0: default,
- uses LLL, 1: uses matrixqz (much slower).
+ x with integral entries. flag is deprecated, and may be set to 0 or 1
+ for backward compatibility.
 Doc: gives an \idx{LLL}-reduced $\Z$-basis
- for the lattice equal to the kernel of the matrix $x$ as columns of the
- matrix $x$ with integer entries (rational entries are not permitted).
+ for the lattice equal to the kernel of the matrix $x$ with rational entries.
 
- If $\fl=0$, uses an integer LLL algorithm.
+ \fl is deprecated, kept for backward compatibility.
 
- If $\fl=1$, uses $\kbd{matrixqz}(x,-2)$. Many orders of magnitude slower
- than the default: never use this.
-
-Variant: See also \fun{GEN}{kerint}{GEN x} ($\fl=0$), which is a trivial
- wrapper around
- \bprog
- ZM_lll(ZM_lll(x, 0.99, LLL_KER), 0.99, LLL_INPLACE);
- @eprog\noindent Remove the outermost \kbd{ZM\_lll} if LLL-reduction is not
- desired (saves time).
+Variant: Use directly \fun{GEN}{kerint}{GEN x} if $x$ is known to have
+ integer entries, and \tet{Q_primpart} first otherwise.

src/test/32/compat
@@ -2191,8 +2191,8 @@ A function with that name existed in GP-1.39.15. Please update your script.
 New syntax: kerint(x) ===> matkerint(x)
 
 matkerint(x,{flag=0}): LLL-reduced Z-basis of the kernel of the matrix x with 
-integral entries. flag is optional, and may be set to 0: default, uses LLL, 1: 
-uses matrixqz (much slower).
+integral entries. flag is deprecated, and may be set to 0 or 1 for backward 
+compatibility.
 
 
   ***   at top-level: kerint1()
@@ -2203,8 +2203,8 @@ A function with that name existed in GP-1.39.15. Please update your script.
 New syntax: kerint1(x) ===> matkerint(x,1)
 
 matkerint(x,{flag=0}): LLL-reduced Z-basis of the kernel of the matrix x with 
-integral entries. flag is optional, and may be set to 0: default, uses LLL, 1: 
-uses matrixqz (much slower).
+integral entries. flag is deprecated, and may be set to 0 or 1 for backward 
+compatibility.
 
 
   ***   at top-level: kerint2()
@@ -4557,4 +4557,4 @@ New syntax: texprint(x) ===> printtex(x)
 printtex({str}*): outputs its string arguments in TeX format.
 
 
-Total time spent: 12
+Total time spent: 22

src/test/32/linear
@@ -270,37 +270,37 @@ Vecsmall([3])
 
 ? matkerint(matrix(4,4,x,y,x*y))
 
-[-1 -1 -1]
+[ 1 -1 -1]
 
-[-1  0  1]
+[ 0 -1  1]
 
-[ 1 -1  1]
+[ 1  1  1]
 
-[ 0  1 -1]
+[-1  0 -1]
 
 ? matkerint(matrix(4,4,x,y,x*y),1)
 
-[-1 -1 -1]
+[ 1 -1 -1]
 
-[-1  0  1]
+[ 0 -1  1]
 
-[ 1 -1  1]
+[ 1  1  1]
 
-[ 0  1 -1]
+[-1  0 -1]
 
 ? matkerint(matrix(4,6,x,y,2520/(x+y)))
 
-[   3    1]
+[ -3   -1]
 
-[ -30  -15]
+[ 30   15]
 
-[  70   70]
+[-70  -70]
 
-[   0 -140]
+[  0  140]
 
-[-126  126]
+[126 -126]
 
-[  84  -42]
+[-84   42]
 
 ? matmuldiagonal(amat,[1,2,3,4,5,6,7])
 
@@ -676,4 +676,4 @@ Vecsmall([4, 3, 2, 1])
 [1, 1/2, 1/3, 1/4, 1/5, 1/6, 1/7, 1/8, 1/9, 1/10]
 ? if(getheap()!=HEAP,getheap())
 ? print("Total time spent: ",gettime);
-Total time spent: 8
+Total time spent: 7

commit d247a96f2b8624d95caad16b649817b33c94d0c4

fix off-by-1 error in matkerint
matker(Mat([1,1])) -> [;]
introduced in c4e44b673

src/basemath/lll.c
@@ -799,7 +799,7 @@ kerint0(GEN M)
 {
   GEN U, H = ZM_hnfall(M,&U,1);
   long d = lg(M)-lg(H);
-  if (d == 1) return cgetg(1, t_MAT);
+  if (!d) return cgetg(1, t_MAT);
   return ZM_lll(vecslice(U,1,d), LLLDFT, LLL_INPLACE);
 }
 GEN

src/test/32/mat
@@ -322,6 +322,11 @@ Mod(18446744073709551628, 18446744073709551629)
 
 [                                         1]
 
+
+[-1]
+
+[ 1]
+
 3
 0
 error("inconsistent dimensions in gtrace.")
@@ -364,4 +369,4 @@ od(0, 18446744073709551629); Mod(7, 18446744073709551629), Mod(8, 1844674407
   ***   at top-level: 1~
   ***                 ^--
   *** _~: incorrect type in gtrans (t_INT).
-Total time spent: 72
+Total time spent: 73

src/test/in/mat
@@ -71,6 +71,7 @@ matdet(A*Mod(1,2^64+13))
 matsolve(A*Mod(1,2^64+13),[1,2,3]~)
 
 matker([1.,I;I,-1.])
+matkerint(Mat([1,1]))
 
 trace(matid(3))
 trace([;])