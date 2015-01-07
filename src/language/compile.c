noloader/pari

commit 271d7b1af0cdce2254b5337cc1e9860653fc5310

introduce varpriority + fix MAXVARN (ab)uses
- varpriority array initialized to priority[i] = -i: allow to use
  pol_x(i) directly.

- fix originally broken quadray

- remove all usage of MAXVARN as a variable number

- add GP function varhigher()

- write "temporary variables" as t<num> rather than #<num>
  [ allow copy-paste ]

- cleanups in entree structs allocation / handling, e.g. remove kludges
  related to findentry.

  FIXME: later switch to plain hashtables
  FIXME: use a different hashtable for member functions and standard symbols.

- introduce full hash in entree structure

- OCstoredyn: don't create variable, don't call checkvalue. Reduces
  drastically the number of created polynomial variables.

  FIXME: do we want to delete an EpINSTALL via f = <newval> ? No for the
  time being.

  FIXME: MAXVARN should now be reduced; e.g. 2^10 polynomial variables
  should be more than enough and will allow to detect leaks sooner.

- don't delete_var() on pari_close

src/language/compile.c
@@ -575,10 +575,10 @@ static entree *
 getfunc(long n)
 {
   long x=tree[n].x;
-  if (tree[x].x==CSTmember)
-    return do_alias(fetch_member(tree[x].str, tree[x].len));
+  if (tree[x].x==CSTmember) /* str-1 points to '.' */
+    return do_alias(fetch_member(tree[x].str - 1, tree[x].len + 1));
   else
-    return do_alias(fetch_entry(tree[x].str, tree[x].len));
+    return do_alias(fetch_entry_raw(tree[x].str, tree[x].len));
 }
 
 static entree *
@@ -1968,8 +1968,8 @@ compilenode(long n, int mode, long flag)
         op_push(OCpushgen,  data_push(strntoGENexp(tree[n].str,tree[n].len)),n);
         break;
       case CSTquote:
-        {
-          entree *ep = fetch_entry(tree[n].str+1,tree[n].len-1);
+        { /* skip ' */
+          entree *ep = fetch_entry_raw(tree[n].str+1,tree[n].len-1);
           if (EpSTATIC(ep)) compile_varerr(tree[n].str+1);
           op_push(OCpushvar, (long)ep,n);
           compilecast(n,Ggen, mode);

COMPAT
@@ -20,6 +20,12 @@ This file lists the incompatible changes between Version 2.x and older versions
   - ellbil(E,P,Q) is now deprecated, use ellheight(E,P,Q)
   - default 'strictmatch' has been obsoleted. It is now a no-op.
   - default 'compatible' has been obsoleted. It is now a no-op.
+  - GP and libpari polynomial variables of arbitrary priority can now be
+    created: 'x' is no longer guaranteed to have maximal priority,
+    nor MAXVARN to have minimal priority.
+  - GP: polynomial variables no longer spring into existence unless a true
+    t_POL/t_SER in the variable is needed. E.g. y = 0 does not create the
+    "variable" y, thereby messing up variable ordering.
 
 * Specific to the PARI library:
 ===============================

doc/usersch2.tex
@@ -1297,7 +1297,9 @@ The answer is that variables are ordered implicitly by the interpreter:
 when a new identifier (e.g~$x$, or $y$ as above) is input, the corresponding
 variable is registered as having a strictly lower priority than any variable in
 use at this point\footnote{*}{This is not strictly true:
-the variable $x$ is predefined and always has the highest possible priority.}%
+the variable $x$ is predefined and initially has the highest possible
+priority. Variables of higher priority than $x$ can be created
+using \tet{varhigher}.}%
 . To see the ordering used by \kbd{gp} at any given time, type
 \kbd{variable()}.\kbdsidx{variable}
 

doc/usersch4.tex
@@ -1573,14 +1573,7 @@ situation.
 Some initializations are needed before you can use a given integer $v$ as a
 variable number.
 
-Initially, this is done for $0$ (the variable \kbd{x} under \kbd{gp}), and
-\tet{MAXVARN}, which is there to address the need for a ``temporary'' new
-variable in library mode and cannot be input under \kbd{gp}. No documented
-library function can create from scratch an object involving \tet{MAXVARN}
-(of course, if the operands originally involve \kbd{MAXVARN}, the function
-abides). We call the latter type a ``temporary variable''. The regular
-variables meant to be used in regular objects, are called ``user
-variables\sidx{variable (user)}''.
+Initially, this is done for $0$ (the variable \kbd{x} under \kbd{gp}).
 
 \subsubsec{User variables}\sidx{variable (user)} When the program starts,
 \kbd{x} is the only user variable (number~$0$). To define new ones, use
@@ -1618,24 +1611,22 @@ that this may not be the same as \kbd{pol\_x(v)} if assignments have been
 performed in the interpreter.
 
 \subsubsec{Temporary variables}\sidx{variable (temporary)}
-\kbd{MAXVARN} is available, but is better left to PARI internal functions
-(some of which do not check that \kbd{MAXVARN} is free for them to use,
-which can be considered a bug). You can create more temporary variables
-using
+You can create temporary variables using
 
 \fun{long}{fetch_var}{}\label{se:fetch_var}
+returns a new variable with \emph{lower} priority than any variable currently
+in use.
+
+\fun{long}{fetch_var_higher}{}\label{se:fetch_var}
+returns a new variable with \emph{higher} priority than any variable
+currently in use.
 
 \noindent
-This returns a variable number which is guaranteed to be unused by the
-library at the time you get it and as long as you do not delete it (we will
-see how to do that shortly). This has \emph{higher} priority than any
-temporary variable produced so far (\kbd{MAXVARN} is assumed to be the first
-such). After the statement \kbd{v = fetch\_var()}, you can use
+After the statement \kbd{v = fetch\_var()}, you can use
 \kbd{pol\_1(v)} and \kbd{pol\_x(v)}. The variables created in this way have no
 identifier assigned to them though, and are printed as
-\kbd{\#<\text{number}>}, except for \kbd{MAXVARN} which is printed
-as~\kbd{\#}. You can assign a name to a temporary variable, after creating
-it, by calling the function
+\kbd{t\text{number}}. You can assign a name to a temporary variable, after
+creating it, by calling the function
 
 \fun{void}{name_var}{long n, char *s}
 
@@ -1643,8 +1634,9 @@ it, by calling the function
 represent the variable number~\kbd{n}. The GP parser will \emph{not}
 recognize it by that name, however, and calling this on a variable known
 to~\kbd{gp} raises an error. Temporary variables are meant to be used as free
-variables, and you should never assign values or functions to them as you
-would do with variables under~\kbd{gp}. For that, you need a user variable.
+variables to build polynomials and power series, and you should never assign
+values or functions to them as you would do with variables under~\kbd{gp}.
+For that, you need a user variable.
 
 All objects created by \kbd{fetch\_var} are on the heap and not on the stack,
 thus they are not subject to standard garbage collecting (they are not
@@ -1654,8 +1646,8 @@ not need a variable number anymore, you can delete it using
 \fun{long}{delete_var}{}
 
 \noindent which deletes the \emph{latest} temporary variable created and
-returns the variable number of the previous one (or simply returns 0 if you
-try, in vain, to delete \kbd{MAXVARN}). Of course you should make sure that
+returns the variable number of the previous one (or simply returns 0 if none
+remain). Of course you should make sure that
 the deleted variable does not appear anywhere in the objects you use later
 on. Here is an example:
 
@@ -1672,8 +1664,7 @@ The (dangerous) statement
 \bprog
   while (delete_var()) /*@Ccom empty */;
 @eprog\noindent
-removes all temporary variables in use, except \kbd{MAXVARN} which cannot be
-deleted.
+removes all temporary variables in use.
 
 \section{Input and output}
 

doc/usersch5.tex
@@ -1316,19 +1316,14 @@ variable we can create.
 \fun{long}{pari_var_next_temp}{} returns \kbd{max\_avail}, the number of the
 next temp variable we can create.
 
-\fun{void}{pari_var_create}{entree *ep} low-level initialization of an
-\kbd{EpVAR}.
+\fun{long}{pari_var_create}{entree *ep} low-level initialization of an
+\kbd{EpVAR}. Return the associated (new) variable number.
 
 \subsec{User variables}
 
 \fun{long}{fetch_user_var}{char *s} returns a user variable whose name
 is \kbd{s}, creating it is needed (and using an existing variable otherwise).
 Returns its variable number.
-
-\fun{entree*}{fetch_named_var}{char *s} as \kbd{fetch\_user\_var}, but
-returns an \kbd{entree*} suitable for inclusion in the interpreter hashlists
-of symbols, not a variable number. \kbd{fetch\_user\_var} is a trivial
-wrapper.
 
 \fun{GEN}{fetch_var_value}{long v} returns a shallow copy of the
 current value of the variable numbered $v$. Return \kbd{NULL} for a temporary
@@ -11420,8 +11415,9 @@ instruction request the iterator to terminate immediately.
 
 \section{Defaults}
 
-\fun{int}{pari_is_default}{const char *s} return $1$ if $s$ is the name
-of a default, $0$ otherwise.
+\fun{entree*}{pari_is_default}{const char *s} return the \kbd{entree}
+structure associated to $s$ if it is the name of a default, \kbd{NULL}
+otherwise.
 
 \fun{GEN}{setdefault}{const char *s, const char *v, long flag} is the
 low-level function underlying \kbd{default0}. If $s$ is \kbd{NULL}, call all

src/basemath/Flx.c
@@ -2711,8 +2711,9 @@ Flxq_charpoly(GEN x, GEN TB, ulong p)
 {
   pari_sp ltop=avma;
   GEN T = get_Flx_mod(TB);
-  GEN xm1 = deg1pol_shallow(pol1_Flx(x[1]),Flx_neg(x,p),evalvarn(MAXVARN));
-  return gerepileupto(ltop, Flx_FlxY_resultant(T, xm1 ,p));
+  GEN xm1 = deg1pol_shallow(pol1_Flx(x[1]),Flx_neg(x,p),evalvarn(fetch_var()));
+  GEN r = Flx_FlxY_resultant(T, xm1, p);
+  (void)delete_var(); return gerepileupto(ltop, r);
 }
 
 GEN

src/basemath/base1.c
@@ -1176,6 +1176,7 @@ nfiso0(GEN a, GEN b, long fliso)
   pari_sp av = avma;
   long i, vb, lx;
   GEN nfa, nfb, y, la, lb;
+  int newvar;
 
   a = get_nfpol(a, &nfa);
   b = get_nfpol(b, &nfb);
@@ -1186,16 +1187,12 @@ nfiso0(GEN a, GEN b, long fliso)
 
   if (nfb) lb = gen_1; else b = ZX_Q_normalize(b,&lb);
   if (nfa) la = gen_1; else a = ZX_Q_normalize(a,&la);
-  a = leafcopy(a); setvarn(a,0);
-  b = leafcopy(b); vb = varn(b);
+  vb = varn(b); newvar = (varncmp(vb,varn(a)) <= 0);
+  if (newvar) { a = leafcopy(a); setvarn(a, fetch_var_higher()); }
   if (nfb)
-  {
-    if (vb == 0) nfb = gsubst(nfb, 0, pol_x(MAXVARN));
     y = lift_intern(nfroots(nfb,a));
-  }
   else
   {
-    if (vb == 0) setvarn(b, fetch_var());
     y = gel(polfnf(a,b),1); lx = lg(y);
     for (i=1; i<lx; i++)
     {
@@ -1203,10 +1200,10 @@ nfiso0(GEN a, GEN b, long fliso)
       if (degpol(t) != 1) { setlg(y,i); break; }
       gel(y,i) = gneg_i(lift_intern(gel(t,2)));
     }
-    if (vb == 0) (void)delete_var();
     settyp(y, t_VEC);
     gen_sort_inplace(y, (void*)&cmp_RgX, &cmp_nodata, NULL);
   }
+  if (newvar) (void)delete_var();
   lx = lg(y); if (lx==1) { avma=av; return gen_0; }
   for (i=1; i<lx; i++)
   {

src/basemath/base2.c
@@ -3771,7 +3771,7 @@ GEN
 nfsplitting(GEN T, GEN D)
 {
   pari_sp av = avma;
-  long d;
+  long d, v;
   GEN F, K;
   T = get_nfpol(T,&K);
   if (!K)
@@ -3797,7 +3797,8 @@ nfsplitting(GEN T, GEN D)
     D = (d <= dmax)? gel(polgalois(T,DEFAULTPREC), 1): mpfact(d);
   }
   d = itos(D);
-  if (varn(T) == 0) K = gsubst(K,0,pol_x(1));
+  v = varn(T);
+  T = leafcopy(T); setvarn(T, fetch_var_higher());
   for(F = T;;)
   {
     GEN P = gel(nffactor(K, F), 1), Q = gel(P,lg(P)-1);
@@ -3805,5 +3806,7 @@ nfsplitting(GEN T, GEN D)
     F = rnfequation(K,Q);
     if (degpol(F) == d) break;
   }
+  (void)delete_var();
+  setvarn(F,v);
   return gerepilecopy(av, F);
 }

commit 09ee0c616e17c7d8a81763807884e65e36170f2f

off-by‐1 error in fetch_member_raw() [#1648]
introduced in 271d7b1a

src/language/compile.c
@@ -575,7 +575,7 @@ static entree *
 fetch_member_raw(const char *s, long len)
 {
   pari_sp av = avma;
-  char *t = stack_malloc(len+1);
+  char *t = stack_malloc(len+2);
   entree *ep;
   t[0] = '_'; strncpy(t+1, s, len); t[++len] = 0; /* prepend '_' */
   ep = fetch_entry_raw(t, len);

src/test/32/ell
@@ -462,4 +462,5 @@ t + 1/3*t^3 - 1/2*t^4 + 14/15*t^5 + O(t^6)
   ***                 ^----------------
   *** ellformalw: domain error in ellformalw: precision <= 0
 242.47010035195076100129810400142304776
-Total time spent: 464
+0
+Total time spent: 512

src/test/in/ell
@@ -277,3 +277,6 @@ U=[-2453699756235148035214202114204024748153737201775184206962152926709240867802
 E=ellinit(U);
 P=[283188771261789732541662405743402980051199109235,39175522019721182331498476245534716069785193060995];
 ellheight(E,P)
+
+\\#1648
+ellinit([0,0,0,0,1]).a1