0b3ba60d4251b94a0ce82f7312269d07e7db98f5
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2015-02-15 15:59:20 +0100

    has_gettimeofday.c: kill warning

--- config/has_gettimeofday.c
+++ config/has_gettimeofday.c
@@ -1,9 +1,8 @@
 #include <stddef.h>
 #include <sys/time.h>
-void main(void)
+int main(void)
 {
   static struct timeval tv0;
-  if(!gettimeofday(&tv0, NULL))
-    return 1;
+  if(!gettimeofday(&tv0, NULL)) return 1;
   return 0;
 }
