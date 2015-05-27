cdb604a4ca0bb6d04da5079e10f60b1325e75d75
Karim Belabas <Karim.Belabas@math.u-bordeaux1.fr>
2015-05-27 09:13:47 +0200

    111- printsep() printed its argument in random format, instead of f_RAW as      print() [#1708]
    + add tests

--- src/language/es.c
+++ src/language/es.c
@@ -4554,11 +4554,11 @@ void
 print0(GEN g, long flag) { out_print0(pariOut, NULL, g, flag); }
 
 void
-printsep(const char *s, GEN g, long flag)
-{ out_print0(pariOut, s, g, flag); pari_putc('\n'); pari_flush(); }
+printsep(const char *s, GEN g)
+{ out_print0(pariOut, s, g, f_RAW); pari_putc('\n'); pari_flush(); }
 
 void
-printsep1(const char *s, GEN g, long flag) { out_print0(pariOut, s, g, flag); }
+printsep1(const char *s, GEN g) { out_print0(pariOut, s, g, f_RAW); }
 
 /* dummy needed to pass a (empty!) va_list to sm_dopr */
 static char *
