ed5662f1e99e0d5b447b17b6fecf96f0b08de9ba
Bill Allombert <Bill.Allombert@math.u-bordeaux1.fr>
2016-09-09 10:19:10 +0200

    Move FpXQX_factor_2 out of FpXQX_factor_i

--- src/basemath/FpXQX_factor.c
+++ src/basemath/FpXQX_factor.c
@@ -1123,6 +1123,29 @@ FpX_factorff(GEN P, GEN T, GEN p)
   return gerepilecopy(av, FpX_factorff_i(P, T, p));
 }
 
+static GEN
+FpXQX_factor_2(GEN f, GEN T, GEN p)
+{
+  long v = varn(f);
+  GEN r = FpXQX_quad_roots(f,T,p);
+  switch(lg(r)-1)
+  {
+  case 0:
+    return mkvec2(mkcolcopy(f), mkvecsmall(1));
+  case 1:
+    return mkvec2(mkcol(deg1pol_shallow(gen_1, Fq_neg(gel(r,1), T, p), v)),
+        mkvecsmall(2));
+  default: /* 2 */
+    {
+      GEN f1 = deg1pol_shallow(gen_1, Fq_neg(gel(r,1), T, p), v);
+      GEN f2 = deg1pol_shallow(gen_1, Fq_neg(gel(r,2), T, p), v);
+      GEN t = mkcol2(f1, f2), E = mkvecsmall2(1, 1);
+      sort_factor_pol(mkvec2(t, E), cmp_RgX);
+      return mkvec2(t, E);
+    }
+  }
+}
+
 /* assumes varncmp (varn(T), varn(f)) > 0 */
 static GEN
 FpXQX_factor_i(GEN f, GEN T, GEN p)
@@ -1138,27 +1161,7 @@ FpXQX_factor_i(GEN f, GEN T, GEN p)
   T = FpX_normalize(T, p);
   f = FpXQX_normalize(f, T, p);
   if (isabsolutepol(f)) return FpX_factorff_i(simplify_shallow(f), T, p);
-  if (degpol(f)==2)
-  {
-    long v = varn(f);
-    GEN r = FpXQX_quad_roots(f,T,p);
-    switch(lg(r)-1)
-    {
-    case 0:
-      return mkvec2(mkcolcopy(f), mkvecsmall(1));
-    case 1:
-      return mkvec2(mkcol(deg1pol_shallow(gen_1, Fq_neg(gel(r,1), T, p), v)),
-                    mkvecsmall(2));
-    case 2:
-      {
-        GEN f1 = deg1pol_shallow(gen_1, Fq_neg(gel(r,1), T, p), v);
-        GEN f2 = deg1pol_shallow(gen_1, Fq_neg(gel(r,2), T, p), v);
-        t = mkcol2(f1, f2); E = mkvecsmall2(1, 1);
-        sort_factor_pol(mkvec2(t, E), cmp_RgX);
-        return mkvec2(t, E);
-      }
-    }
-  }
+  if (degpol(f)==2) return FpXQX_factor_2(f, T, p);
 
   pg = itos_or_0(p);
   df2  = NULL; /* gcc -Wall */
