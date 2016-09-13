8f957e695271ecd46d95a5ad81dc591a10879220
Karim Belabas <Karim.Belabas@math.u-bordeaux.fr>
2016-09-13 06:00:35 +0200

    streamline idealnumdem
    - don't compute idealtwoelt(idealhnf(principal ideal))
    - d = 1 is impossible at the end
    - often c == d

--- src/basemath/base4.c
+++ src/basemath/base4.c
@@ -1658,7 +1658,7 @@ GEN
 idealnumden(GEN nf, GEN x)
 {
   pari_sp av = avma;
-  GEN ax, c, d, A, B, J;
+  GEN x0, ax, c, d, A, B, J;
   long tx = idealtyp(&x,&ax);
   nf = checknf(nf);
   switch (tx)
@@ -1666,24 +1666,29 @@ idealnumden(GEN nf, GEN x)
     case id_PRIME:
       retmkvec2(idealhnf(nf, x), gen_1);
     case id_PRINCIPAL:
+    {
+      GEN xZ, mx;
       x = nf_to_scalar_or_basis(nf, x);
       switch(typ(x))
       {
-        case t_INT:
-          return gerepilecopy(av, mkvec2(absi(x),gen_1));
-        case t_FRAC:
-          return gerepilecopy(av, mkvec2(absi(gel(x,1)), gel(x,2)));
+        case t_INT: return gerepilecopy(av, mkvec2(absi(x),gen_1));
+        case t_FRAC:return gerepilecopy(av, mkvec2(absi(gel(x,1)), gel(x,2)));
       }
       /* t_COL */
       x = Q_remove_denom(x, &d);
       if (!d) return gerepilecopy(av, mkvec2(idealhnf(nf, x), gen_1));
-      x = idealhnf(nf, x);
+      mx = zk_multable(nf, x);
+      xZ = zkmultable_capZ(mx);
+      x = ZM_hnfmodid(mx, xZ); /* principal ideal (x) */
+      x0 = mkvec2(xZ, mx); /* same, for fast multiplication */
       break;
-    case id_MAT: {
+    }
+    default: /* id_MAT */
+    {
       long n = lg(x)-1;
       if (n == 0) return mkvec2(gen_0, gen_1);
       if (n != nf_get_degree(nf)) pari_err_DIM("idealnumden");
-      x = Q_remove_denom(x, &d);
+      x0 = x = Q_remove_denom(x, &d);
       if (!d) return gerepilecopy(av, mkvec2(x, gen_1));
       break;
     }
@@ -1691,10 +1696,9 @@ idealnumden(GEN nf, GEN x)
   J = hnfmodid(x, d); /* = d/B */
   c = gcoeff(J,1,1); /* (d/B) \cap Z, divides d */
   B = idealinv_HNF_Z(nf, J); /* (d/B \cap Z) B/d */
-  c = diviiexact(d, c);
-  if (!is_pm1(c)) B = ZM_Z_mul(B, c); /* = B ! */
-  A = idealmul(nf, x, B); /* d * (original x) * B = d A */
-  if (!is_pm1(d)) A = ZM_Z_divexact(A, d); /* = A ! */
+  if (!equalii(c,d)) B = ZM_Z_mul(B, diviiexact(d,c)); /* = B ! */
+  A = idealmul_HNF(nf, B, x0); /* d * (original x) * B = d A */
+  A = ZM_Z_divexact(A, d); /* = A ! */
   return gerepilecopy(av, mkvec2(A, B));
 }
 
