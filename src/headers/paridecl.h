8040d37f37d8febe619600d0a1956aa4bf146511
Bill Allombert <Bill.Allombert@math.u-bordeaux1.fr>
2016-11-21 12:21:16 +0100

    document ZM_ker_ratlift

--- src/headers/paridecl.h
+++ src/headers/paridecl.h
@@ -1411,6 +1411,7 @@ void    RgMs_structelim(GEN M, long nbrow, GEN A, GEN *p_col, GEN *p_lin);
 GEN     ZM_det(GEN a);
 GEN     ZM_detmult(GEN A);
 GEN     ZM_gauss(GEN a, GEN b);
+GEN     ZM_ker_ratlift(GEN M);
 GEN     ZM_imagecompl(GEN x);
 GEN     ZM_indeximage(GEN x);
 GEN     ZM_indexrank(GEN x);
