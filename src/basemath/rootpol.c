bfa3ba49638bd25a8a48423f8608ee1e11ef8f16
Karim Belabas <Karim.Belabas@math.u-bordeaux.fr>
2017-03-31 22:34:40 +0200

    64- polroots(t_POL whose coeffs vary by a factor > 2^100000) -> SEGV
    (or stack overflow)

--- src/basemath/rootpol.c
+++ src/basemath/rootpol.c
@@ -20,7 +20,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
 #include "pari.h"
 #include "paripriv.h"
 
-static const double pariINFINITY = 100000.;
+static const double pariINFINITY = 1./0.;
 
 static long
 isvalidcoeff(GEN x)
