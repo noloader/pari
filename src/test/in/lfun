noloader/pari

commit 90ad7249fa92751ca02a82bb1d3e9b91535f1258

31- lfuncreate() with (p,d)->Lp closure and bad primes [p,Lp]
must now use the actual local factor and not its inverse

src/basemath/lfunutils.c
@@ -259,11 +259,11 @@ static GEN
 localfactor(void *E, GEN p, long n)
 {
   GEN a = (GEN)E;
-  GEN s = ginv(closure_callgen2(a, p, stoi(n-1)));
+  GEN s = closure_callgen2(a, p, stoi(n-1));
   s = gtoser(s, gvar(s), n);
   if (signe(s)==0 || valp(s) || !gequal1(gel(s, 2)))
     pari_err_DOMAIN("direuler","constant term","!=", gen_1,s);
-  return gtrunc(s);
+  return s;
 }
 static GEN
 vecan_closure(GEN a, long L, GEN Sbad)
@@ -288,7 +288,7 @@ vecan_closure(GEN a, long L, GEN Sbad)
   v = gel(a,1); tv = typ(v);
   /* regular vector, return it */
   if (tv != t_CLOSURE && tv != t_VEC) return vecslice(a, 1, minss(L,la-1));
-  /* vector [an, [p1, 1/L_{p1}], ..., [pk, 1/L_{pk}}]]: exceptional primes */
+  /* vector [an, [p1, L_{p1}], ..., [pk, L_{pk}}]]: exceptional primes */
   if (la > 1) Sbad = vecslice(a, 2, la-1);
   return vecan_closure(v, L, Sbad);
 }
@@ -1217,7 +1217,6 @@ ellfromeqncharpoly(GEN P, GEN Q, GEN p)
 static GEN
 genus2_eulerfact(GEN P, GEN p)
 {
-  pari_sp av = avma;
   GEN Pp = FpX_red(P, p);
   GEN GU = genus2_redmodel(Pp, p);
   long d = 6-degpol(Pp), v = d/2, w = odd(d);
@@ -1256,7 +1255,7 @@ genus2_eulerfact(GEN P, GEN p)
     kq = gmul(kq, kqoo);
   }
   tor = RgX_div(ZX_mul(oneminusxd(1), kq), ZX_mul(ki, kp));
-  return gerepileupto(av, ZX_mul(abe, tor));
+  return ginv( ZX_mul(abe, tor) );
 }
 
 static GEN
@@ -1365,7 +1364,6 @@ genus2_redmodel2(GEN P)
 static GEN
 genus2_eulerfact2(GEN PQ)
 {
-  pari_sp av = avma;
   GEN V = F2x_genus_red(ZX_to_F2x(gel(PQ, 1)), ZX_to_F2x(gel(PQ, 2)));
   GEN P = gel(V, 1), Q = gel(V, 2);
   GEN F = gel(V, 3), v = gel(V, 4);
@@ -1403,7 +1401,7 @@ genus2_eulerfact2(GEN PQ)
     kq = gmul(kq, kqoo);
   }
   tor = RgX_div(ZX_mul(oneminusxd(1),kq), ZX_mul(ki, kp));
-  return gerepileupto(av, ZX_mul(abe, tor));
+  return ginv( ZX_mul(abe, tor) );
 }
 
 GEN
@@ -1421,7 +1419,7 @@ lfungenus2(GEN G)
     pari_warn(warner,"unknown valuation of conductor at 2");
   e = cgetg(lL+(ram2?0:1), t_VEC);
   gel(e,1) = mkvec2(gen_2, ram2 ? genus2_eulerfact2(PQ)
-           : RgX_recip(hyperellcharpoly(gmul(PQ,gmodulss(1,2)))));
+           : ginv( RgX_recip(hyperellcharpoly(gmul(PQ,gmodulss(1,2))))) );
   for(i = ram2? 2: 1; i < lL; i++)
   {
     GEN Li = gel(L, i);
@@ -1728,7 +1726,7 @@ artin_ram(GEN nf, GEN gal, GEN pr, GEN ramg, GEN ch, long d)
   gel(V,2) = gen_0;
   for(i=1; i<=d; i++)
   {
-    gel(V,i+2) = gdivgs(artin_ind(elts, ch, q), -i);
+    gel(V,i+2) = gdivgs(artin_ind(elts, ch, q), i);
     q = gmul(q, p);
   }
   delete_var();
@@ -1736,7 +1734,7 @@ artin_ram(GEN nf, GEN gal, GEN pr, GEN ramg, GEN ch, long d)
   setvarn(V,0); return gerepilecopy(av,V);
 }
 
-/* [Artin conductor, vec of [p, Lp^(-1)]] */
+/* [Artin conductor, vec of [p, Lp]] */
 static GEN
 artin_badprimes(GEN N, GEN G, GEN ch)
 {

src/test/in/lfun
@@ -116,18 +116,17 @@ L[5]*=4;
 lfuncheckfeq(L)
 
 print("Curve y^2=",x^5+x);
-fun(p)=
+fun(p, d)= \\ if (d < 2), only 1 + trace term is needed
 {
-  my(p16=p%16,p8=p16%8,c,d);
-  if (p8==1||p8==3,
-   [c,d]=qfbsolve(Qfb(1,0,2),p);
-   if (c%4!=1,c=-c));
-  if(p16==1,(x^2-2*x*c+p)^2,
-     p16==9,(x^2+2*x*c+p)^2,
-     p8==3, (x^2+2*x*c+p)*(x^2-2*x*c+p),
-     p8==5 || p8==7,(x^2+p)^2);
+  my(p16=p%16, p8=p16%8);
+  if (d < 2 && p8 != 1, return (1));
+  if (p8 == 5 || p8 == 7, return((1+p*x^2)^-2));
+  my([c,d] = qfbsolve(Qfb(1,0,2),p));
+  if (p8 == 3, return (1 / (1 + (-4*c^2 + 2*p)*x^2 + p^2*x^4)));
+  if (c%4 != 1, c=-c);
+  if(p16==1,(1-2*x*c+p*x^2)^-2, (1+2*x*c+p*x^2)^-2);
 }
-L=lfuncreate([[(p,d)->polrecip(fun(p)),[2, 1]], 0, [0,0,1,1], 2, 2^16, -1]);
+L=lfuncreate([[(p,d)->fun(p, d),[2, 1]], 0, [0,0,1,1], 2, 2^16, -1]);
 default("realprecision",38)
 lfuncheckfeq(L)
 lfunrootres(L)
@@ -135,17 +134,23 @@ lfun(L,1,1)
 
 print("Curve y^2=",x^5+1);
 B=bnfinit(polcyclo(5));
-fun2(p)=
+fun2(p, d)= \\ if (d < 2), only 1 + trace term is needed
 {
-  my(p10=p%10);
-  if (p10==3 || p10==7,x^4+p^2,
-      p10==9, x^4+2*p*x^2+p^2,
-      my(u=bnfisintnorm(B,p)[1], a = Mod(subst(u,x,x^2),B.pol)*u);
-      select(z->polcoeff(z,3)%10==6,
-        vector(10,i,minpoly((-x)^(i-1)*a)))[1]);
+  my(p10 = p%10);
+  if (p10 != 1,
+    if (d < 2, return (1));
+    return (if (p10==9, 1 / (1+2*p*x^2+p^2*x^4), 1 / (1 + p^2*x^4)));
+  );
+  my (u = nfbasistoalg(B, bnfisprincipal(B,idealprimedec(B,p)[1])[2]));
+  my (a = subst(liftpol(u),x,x^2) * u);
+  for (i = 0, 9,
+    my (b = (-x)^i * a);
+    my (t = nfelttrace(B, b), t10 = t % 10);
+    if (t10 == 4, return(if (d < 2, 1 + t*x, 1 / polrecip(minpoly(b)))));
+  );
 }
 
-L=lfuncreate([[(p,d)->polrecip(fun2(p)),[2, 1],[5, 1]], 0, [0,0,1,1], 2, 5^4*2^4, 1]);
+L=lfuncreate([[(p,d)->fun2(p,d),[2, 1],[5, 1]], 0, [0,0,1,1], 2, 5^4*2^4, 1]);
 lfuncheckfeq(L)
 lfunrootres(L)
 lfun(L,1)
@@ -179,11 +184,14 @@ localbitprec(4); lfuncheckfeq(E)
 localbitprec(32);lfuncheckfeq(E)
 
 print("Grossencharacter");
-
 K=bnfinit(polcyclo(5));
 id5=idealprimedec(K,5)[1];
 B=bnrinit(K,id5,1);
-F(p)=my(pr=idealprimedec(K,p)); simplify(lift(prod(k=1,#pr,(1-G(pr[k])*X^pr[k].f))));
+F(p,d)=
+{
+  my(pr=idealprimedec(K, p, d));
+  1 / simplify(lift(prod(k=1,#pr, 1 - G(pr[k])*X^pr[k].f)));
+}
 G(pr)=
 {
   if(idealval(K,B.mod,pr),0,
@@ -193,7 +201,7 @@ G(pr)=
     my(al3=nfbasistoalg(K,nfgaloisapply(K,x^3,al)));
     al1^3*al2^2*al3^1);
 }
-L=lfuncreate([(p,n)->F(p),0,[-1,0,0,1],4,625,0]);
+L=lfuncreate([(p,d)->F(p,d),0,[-1,0,0,1],4,625,0]);
 lfuncheckfeq(L)
 lfun(L,2)/(Pi/5^(15/4)*gamma(1/5)^(7/2)*gamma(3/5)^(1/2)/(gamma(4/5)^(7/2)*gamma(2/5)^(1/2)))
 
@@ -202,10 +210,10 @@ default("realprecision",54)
 e = ellinit([0, 0, 1, -1, 0]);
 le2 = lfuncreate([ n->ellan(e,n), 0, [0,1], 2, 37, -1]);
 le3 = lfuncreate([ ellan(e,1000), 0, [0,1], 2, 37, -1]);
-le4 = lfuncreate([ (p,d)->1-ellap(e,p)*'x+if(p==37,0,p*'x^2), 0, [0,1], 2, 37, -1]);
+le4 = lfuncreate([ (p,d)->1/(1-ellap(e,p)*'x+if(p!=37,p*'x^2)), 0, [0,1], 2, 37, -1]);
 le21 = lfuncreate([ [n->ellan(e,n),[37,1+'x]], 0, [0,1], 2, 37, -1]);
 le31 = lfuncreate([ [ellan(e,1000),[37,1+'x]], 0, [0,1], 2, 37, -1]);
-le41 = lfuncreate([ [(p,d)->1-ellap(e,p)*'x+p*'x^2, [37,1+'x]], 0, [0,1], 2, 37, -1]);
+le41 = lfuncreate([ [(p,d)->1/(1-ellap(e,p)*'x+p*'x^2), [37,1/(1+'x)]], 0, [0,1], 2, 37, -1]);
 lfuncheckfeq(le2)
 lfuncheckfeq(le3)
 lfuncheckfeq(le4)
@@ -248,7 +256,7 @@ lfuncheckfeq(L)
 
 print("zeta(s-a)");
 
-Z(a)=lfuncreate([(p,n)->(1-p^a*x),0,[-a],2*a+1,1,1,[[a+1,1/x+O(1)]]]);
+Z(a)=lfuncreate([(p,n)->1/(1-p^a*x),0,[-a],2*a+1,1,1,[[a+1,1/x+O(1)]]]);
 lfuncheckfeq(Z(1))
 lfun(Z(1),2)
 lfun(Z(1),3)
@@ -258,7 +266,7 @@ lfun(Z(2),3)
 
 print("zeta(s)*zeta(s-a)");
 
-L(a)=lfuncreate([(p,n)->(1-p^a*x)*(1-x),0,[-a,0],a+1,1,1,[[a+1,lfun(1,a+1+x+O(x^2))*(1/x+Euler+O(x))],[1,lfun(1,1-a+x+O(x^3))*(1/x+Euler+O(x))]]]);
+L(a)=lfuncreate([(p,n)->1/((1-p^a*x)*(1-x)),0,[-a,0],a+1,1,1,[[a+1,lfun(1,a+1+x+O(x^2))*(1/x+Euler+O(x))],[1,lfun(1,1-a+x+O(x^3))*(1/x+Euler+O(x))]]]);
 lfuncheckfeq(L(1))
 lfun(L(1),2)
 lfun(L(1),3)

COMPAT
@@ -6,6 +6,8 @@ This file lists the incompatible changes between Version 2.x and older versions
     vectors)
   - sumnummonieninit(a) for [a,a] did not conform to documentation.
     Change to sumnummonieninit(b) for [1,b]
+  - lfuncreate() with (p,d)->Lp closure and bad primes [p,Lp]: must now
+    use the actual local factor and not its inverse
 
 * Specific to the PARI library:
 ===============================

src/basemath/dirichlet.c
@@ -118,8 +118,8 @@ direuler_bad(void *E, GEN (*eval)(void *, GEN, long d), GEN a, GEN b, GEN c, GEN
       GEN ai = gel(Sbad,i), s;
       if (typ(ai) != t_VEC || lg(ai) != 3)
         pari_err_TYPE("direuler [bad primes]",ai);
-      q = gtou(gel(ai, 1));
-      s = ginv(gel(ai, 2));
+      q = gtou(gel(ai,1));
+      s = gel(ai,2);
       if (q <= cu)
       {
         long nl = ulogint(cu, q) + 1;

src/functions/l_functions/lfuncreate
@@ -42,13 +42,13 @@ Doc: This low-level routine creates \tet{Ldata} structures, needed by
 
  A second format is limited to multiplicative $L$ functions affording an
  Euler product. It is a closure of arity 2 \kbd{(p,d)->L(p)} giving the
- inverse of the local
- factor $L_p$ at $p$ as a rational function, to be evaluated at $p^{-s}$ as in
- \kbd{direuler}; $d$ is set to the floor of $\log_p(n)$, where $n$ is the
- total number of Dirichlet coefficients $(a_1,\dots,a_n)$ that will be
- computed in this way. This parameter $d$ allows to compute only part of $L_p$
- when $p$ is large and $L_p$ expensive to compute, but it can of course be
- ignored by the closure.
+ local factor $L_p(X)$ at $p$ as a rational function, to be evaluated at
+ $p^{-s}$ as in \kbd{direuler}; $d$ is set to \kbd{logint}$(n,p)$, where
+ $n$ is the total number of Dirichlet coefficients $(a_1,\dots,a_n)$ that will
+ be computed in this way. This parameter $d$ allows to compute only part of
+ $L_p$ when $p$ is large and $L_p$ expensive to compute: any polynomial
+ (or \typ{SER}) congruent to $L_p$ modulo $X^{d+1}$ is acceptable since only the coefficients of $X^0, \dots, X^d$ are needed to expand the Dirichlet series.
+ The closure can of course ignore this parameter.
  \bprog
  ? z = lfuncreate([(p,d)->1-x, 1, [0], 1, 1, 1, 1]); \\ Riemann zeta
  ? lfun(z,2) - Pi^2/6
@@ -56,11 +56,11 @@ Doc: This low-level routine creates \tet{Ldata} structures, needed by
  @eprog
 
  Finally one can describe separately the generic Dirichlet coefficients
- and the bad local factors by setting $\kbd{dir} = [an, [p_1,L^{-1}_{p_1}],
- \dots,[p_k,L^{-1}_{p_k}]]$, where \kbd{an} describes the generic coefficients
+ and the bad local factors by setting $\kbd{dir} = [an, [p_1,L_{p_1}],
+ \dots,[p_k,L_{p_k}]]$, where \kbd{an} describes the generic coefficients
  in one of the two formats above, except that coefficients $a_n$ with
- $p_i \mid n$ for some $i \leq k$ will be ignored. The subsequent pairs $[p,
- L_p^{-1}]$ give the bad primes and corresponding \emph{inverse} local
+ $p_i \mid n$ for some $i \leq k$ will be ignored. The subsequent pairs
+ $[p, L_p]$ give the bad primes and corresponding local
  factors.
 
  \item \kbd{V[2]=astar} is the Dirichlet series coefficients of the dual

src/test/in/lfuntype
@@ -1,6 +1,6 @@
 default(realprecision,38);
 zet=1;
-Z(a)=lfuncreate([(p,n)->(1-p^a*x),0,[-a],2*a+1,1,1,[[a+1,1/x+O(1)]]]);
+Z(a)=lfuncreate([(p,n)->1/(1-p^a*x),0,[-a],2*a+1,1,1,[[a+1,1/x+O(1)]]]);
 N=nfinit(x^10-2*x^8-9*x^6+57*x^4-69*x^2+47);
 G=galoisinit(N);
 VALL={

commit 06caec2605be5c90f5fb585ba05b35b82f2f1e66

artin_ram: fix bug introduced in 90ad7249f
The Euler factor at bad primes was truncated.

src/basemath/lfunutils.c
@@ -1726,12 +1726,12 @@ artin_ram(GEN nf, GEN gal, GEN aut, GEN pr, GEN ramg, GEN ch, long d)
   gel(V,2) = gen_0;
   for(i=1; i<=d; i++)
   {
-    gel(V,i+2) = gdivgs(artin_ind(elts, ch, q), i);
+    gel(V,i+2) = gdivgs(artin_ind(elts, ch, q), -i);
     q = gmul(q, p);
   }
   delete_var();
   V = RgXn_exp(V,d+1);
-  setvarn(V,0); return gerepilecopy(av,V);
+  setvarn(V,0); return gerepilecopy(av,ginv(V));
 }
 
 /* [Artin conductor, vec of [p, Lp]] */

src/test/32/lfun
@@ -231,6 +231,7 @@ zeta(s)*zeta(s-a)
 725.0000000000000000
 24217.00000000000000
 28614069.00000000000
+-62
 1
 [6, 184]
 [[12, 125], [11, 125], [5, 124]]
@@ -254,4 +255,4 @@ zeta(s)*zeta(s-a)
   ***   at top-level: lfunan(lfuncreate([1,0,[0],1,1,1,1]),10)
   ***                 ^----------------------------------------
   *** lfunan: incorrect type in vecan_closure (t_INT).
-Total time spent: 5256
+Total time spent: 4232

src/test/in/lfun
@@ -321,6 +321,11 @@ N=nfinit(x^6-3*x^5+5*x^4-5*x^3+5*x^2-3*x+1);G=galoisinit(N);
 L=lfunartin(N,G,[[-1,1;-1,0],[0,1;1,0]],1);
 if(lfunan(L,100)!=lfunan(lfundiv(x^3-3*x^2+2*x-1,1),100),error("lfunartin"));
 
+N=nfinit(x^12-11*x^10+44*x^8-78*x^6+60*x^4-16*x^2+1);
+G=galoisinit(N);
+L=lfunartin(N,G,[1,-x+1,x,-x+1,-1,x,-x,1,x-1,-x,x-1,-1]~,6);
+lfuncheckfeq(L)
+
 localbitprec(20); #lfunzeros('x^2-2,6)
 
 lfuncost(1, [100], 5)