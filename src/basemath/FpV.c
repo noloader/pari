noloader/pari

commit aa2157c70a5b3e492edc23eeeaa4be040a011d98

Add Flx_dotproduct

src/basemath/FpV.c
@@ -646,11 +646,11 @@ FpV_dotsquare(GEN x, GEN p)
 }
 
 INLINE ulong
-Flv_dotproduct_SMALL(GEN x, GEN y, ulong p, long lx)
+Flv_dotproductspec_SMALL(GEN x, GEN y, ulong p, long lx)
 {
-  ulong c = uel(x,1) * uel(y,1);
+  ulong c = uel(x,0) * uel(y,0);
   long k;
-  for (k = 2; k < lx; k++) {
+  for (k = 1; k < lx; k++) {
     c += uel(x,k) * uel(y,k);
     if (c & HIGHBIT) c %= p;
   }
@@ -658,10 +658,10 @@ Flv_dotproduct_SMALL(GEN x, GEN y, ulong p, long lx)
 }
 
 INLINE ulong
-Flv_dotproduct_i(GEN x, GEN y, ulong p, ulong pi, long lx)
+Flv_dotproductspec_i(GEN x, GEN y, ulong p, ulong pi, long lx)
 {
   ulong l0, l1, v1, h0, h1;
-  long i = 1;
+  long i = 0;
   LOCAL_OVERFLOW;
   LOCAL_HIREMAINDER;
   l1 = mulll(uel(x,i), uel(y,i)); h1 = hiremainder; v1 = 0;
@@ -676,23 +676,34 @@ Flv_dotproduct_i(GEN x, GEN y, ulong p, ulong pi, long lx)
 ulong
 Flv_dotproduct(GEN x, GEN y, ulong p)
 {
-  long lx = lg(x);
+  long lx = lg(x)-1;
   if (lx == 1) return 0;
   if (SMALL_ULONG(p))
-    return Flv_dotproduct_SMALL(x, y, p, lx);
+    return Flv_dotproductspec_SMALL(x+1, y+1, p, lx);
   else
-    return Flv_dotproduct_i(x, y, p, get_Fl_red(p), lx);
+    return Flv_dotproductspec_i(x+1, y+1, p, get_Fl_red(p), lx);
 }
 
 ulong
 Flv_dotproduct_pre(GEN x, GEN y, ulong p, ulong pi)
 {
-  long lx = lg(x);
+  long lx = lg(x)-1;
   if (lx == 1) return 0;
   if (SMALL_ULONG(p))
-    return Flv_dotproduct_SMALL(x, y, p, lx);
+    return Flv_dotproductspec_SMALL(x+1, y+1, p, lx);
   else
-    return Flv_dotproduct_i(x, y, p, pi, lx);
+    return Flv_dotproductspec_i(x+1, y+1, p, pi, lx);
+}
+
+ulong
+Flx_dotproduct(GEN x, GEN y, ulong p)
+{
+  long lx = minss(lgpol(x), lgpol(y));
+  if (lx == 0) return 0;
+  if (SMALL_ULONG(p))
+    return Flv_dotproductspec_SMALL(x+2, y+2, p, lx);
+  else
+    return Flv_dotproductspec_i(x+2, y+2, p, get_Fl_red(p), lx);
 }
 
 ulong

doc/usersch5.tex
@@ -5039,6 +5039,9 @@ assuming $pi$ is the pseudo inverse of $p$.
 \fun{ulong}{Flx_Flv_multieval}{GEN P, GEN v, ulong y, ulong p} returns the vector
 $[P(v[1]),\ldots,P(v[n])]$ as a \kbd{Flv}.
 
+\fun{ulong}{Flx_dotproduct}{GEN x, GEN y, ulong p} returns the scalar product
+of the coefficients of $x$ and $y$.
+
 \fun{GEN}{Flx_deflate}{GEN P, long d} assuming $P$ is a polynomial of the
 form $Q(X^d)$, return $Q$.
 

src/headers/paridecl.h
@@ -682,6 +682,7 @@ void    Flv_neg_inplace(GEN v, ulong p);
 GEN     Flv_sub(GEN x, GEN y, ulong p);
 void    Flv_sub_inplace(GEN x, GEN y, ulong p);
 ulong   Flv_sum(GEN x, ulong p);
+ulong   Flx_dotproduct(GEN x, GEN y, ulong p);
 GEN     Fp_to_mod(GEN z, GEN p);
 GEN     FpC_FpV_mul(GEN x, GEN y, GEN p);
 GEN     FpC_Fp_mul(GEN x, GEN y, GEN p);

commit 06137d61cee67a9d27f26b15ff7a8810a99a6984

Flv_dotproduct broken in aa2157c7

src/basemath/FpV.c
@@ -740,7 +740,7 @@ ulong
 Flv_dotproduct(GEN x, GEN y, ulong p)
 {
   long lx = lg(x)-1;
-  if (lx == 1) return 0;
+  if (lx == 0) return 0;
   if (SMALL_ULONG(p))
     return Flv_dotproductspec_SMALL(x+1, y+1, p, lx);
   else
@@ -751,7 +751,7 @@ ulong
 Flv_dotproduct_pre(GEN x, GEN y, ulong p, ulong pi)
 {
   long lx = lg(x)-1;
-  if (lx == 1) return 0;
+  if (lx == 0) return 0;
   if (SMALL_ULONG(p))
     return Flv_dotproductspec_SMALL(x+1, y+1, p, lx);
   else
@@ -774,9 +774,9 @@ F2v_dotproduct(GEN x, GEN y)
 {
   long i, lx = lg(x);
   ulong c;
-  if (lx == 2) return 0;
-  c = uel(x,2) & uel(y,2);
-  for (i=3; i<lx; i++) c ^= uel(x,i) & uel(y,i);
+  if (lx == 1) return 0;
+  c = uel(x,1) & uel(y,1);
+  for (i=2; i<lx; i++) c ^= uel(x,i) & uel(y,i);
 #ifdef LONG_IS_64BIT
   c ^= c >> 32;
 #endif