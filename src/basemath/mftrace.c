8c86f92aef3d247d84a0f36bdf175ca407a89bc5
Karim Belabas <Karim.Belabas@math.u-bordeaux.fr>
2017-06-30 17:58:38 +0200

    mfpackage: reword 'not in this space' error message

--- src/basemath/mftrace.c
+++ src/basemath/mftrace.c
@@ -7505,8 +7505,8 @@ mfisinspace_i(GEN mf, GEN F)
 static void
 err_space(GEN F)
 {
-  pari_err_DOMAIN("mftobasis", "form", "not in",
-                  strtoGENstr("this modular form space"), F);
+  pari_err_DOMAIN("mftobasis", "form", "does not belong to",
+                  strtoGENstr("space"), F);
 }
 /* when flag set, do not return error message */
 GEN
