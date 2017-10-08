6422fa7f2e77dc22ab05d5ccba0aa3715ff9ba2b
Bill Allombert <Bill.Allombert@math.u-bordeaux1.fr>
2017-10-08 18:18:16 +0200

    gen_crt_Z: move to paripriv.h

--- src/headers/paripriv.h
+++ src/headers/paripriv.h
@@ -161,6 +161,7 @@ GEN  ZX_ZXY_resultant_worker(GEN A, GEN B, ulong dp, ulong p, GEN dB);
 GEN  ZM_det_worker(GEN P, GEN A);
 GEN  aprcl_step4_worker(ulong q, GEN pC, GEN N, GEN v);
 GEN  aprcl_step6_worker(GEN r, long t, GEN N, GEN N1, GEN et);
+GEN  gen_crt_Z(const char *s, GEN worker, GEN dB, ulong bound, long mmin, GEN *pt_mod);
 
 /* Relative number fields */
 enum { rnf_NFABS = 1, rnf_MAPS };
