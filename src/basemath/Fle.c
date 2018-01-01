68d7a11014d6dee04013b376d9c16e165abcc825
Karim Belabas <Karim.Belabas@math.u-bordeaux.fr>
2018-01-01 21:42:03 +0100

    whitespace edits

--- src/basemath/Fle.c
+++ src/basemath/Fle.c
@@ -26,14 +26,10 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. */
  * a projective point (x : y : z) on E by [z*x , z^2*y , z].  This is
  * probably not the fastest representation available for the given
  * problem, but they're easy to implement and up to 60% faster than
- * the school-book method used in Fle_mulu().
- */
+ * the school-book method used in Fle_mulu(). */
 
-/*
- * Cost: 1M + 8S + 1*a + 10add + 1*8 + 2*2 + 1*3.
- * Source: http://www.hyperelliptic.org/EFD/g1p/auto-shortw-jacobian.html#doubling-dbl-2007-bl
- */
-
+/* Cost: 1M + 8S + 1*a + 10add + 1*8 + 2*2 + 1*3.
+ * Source: http://www.hyperelliptic.org/EFD/g1p/auto-shortw-jacobian.html#doubling-dbl-2007-bl */
 INLINE void
 Flj_dbl_indir_pre(GEN P, GEN Q, ulong a4, ulong p, ulong pi)
 {
@@ -42,11 +38,7 @@ Flj_dbl_indir_pre(GEN P, GEN Q, ulong a4, ulong p, ulong pi)
 
   X1 = P[1]; Y1 = P[2]; Z1 = P[3];
 
-  if (Z1 == 0)
-  {
-    Q[1] = X1; Q[2] = Y1; Q[3] = Z1;
-    return;
-  }
+  if (Z1 == 0) { Q[1] = X1; Q[2] = Y1; Q[3] = Z1; return; }
 
   XX = Fl_sqr_pre(X1, p, pi);
   YY = Fl_sqr_pre(Y1, p, pi);
@@ -77,11 +69,8 @@ Flj_dbl_pre(GEN P, ulong a4, ulong p, ulong pi)
   return Q;
 }
 
-/*
- * Cost: 11M + 5S + 9add + 4*2.
- * Source: http://www.hyperelliptic.org/EFD/g1p/auto-shortw-jacobian.html#addition-add-2007-bl
- */
-
+/* Cost: 11M + 5S + 9add + 4*2.
+ * Source: http://www.hyperelliptic.org/EFD/g1p/auto-shortw-jacobian.html#addition-add-2007-bl */
 INLINE void
 Flj_add_indir_pre(GEN P, GEN Q, GEN R, ulong a4, ulong p, ulong pi)
 {
@@ -90,15 +79,8 @@ Flj_add_indir_pre(GEN P, GEN Q, GEN R, ulong a4, ulong p, ulong pi)
   X1 = P[1]; Y1 = P[2]; Z1 = P[3];
   X2 = Q[1]; Y2 = Q[2]; Z2 = Q[3];
 
-  if (Z2 == 0) {
-    R[1] = X1; R[2] = Y1; R[3] = Z1;
-    return;
-  }
-
-  if (Z1 == 0) {
-    R[1] = X2; R[2] = Y2; R[3] = Z2;
-    return;
-  }
+  if (Z2 == 0) { R[1] = X1; R[2] = Y1; R[3] = Z1; return; }
+  if (Z1 == 0) { R[1] = X2; R[2] = Y2; R[3] = Z2; return; }
 
   Z1Z1 = Fl_sqr_pre(Z1, p, pi);
   Z2Z2 = Fl_sqr_pre(Z2, p, pi);
@@ -109,15 +91,9 @@ Flj_add_indir_pre(GEN P, GEN Q, GEN R, ulong a4, ulong p, ulong pi)
   H = Fl_sub(U2, U1, p);
   r = Fl_double(Fl_sub(S2, S1, p), p);
 
-  /* If points are equal we must double. */
   if (H == 0) {
-    if (r == 0) {
-      /* Points are equal so double. */
-      Flj_dbl_indir_pre(P, R, a4, p, pi);
-    } else {
-      /* Points are opposite so return zero. */
-      R[1] = R[2] = 1; R[3] = 0;
-    }
+    if (r == 0) Flj_dbl_indir_pre(P, R, a4, p, pi); /*Points are equal, double*/
+    else { R[1] = R[2] = 1; R[3] = 0; } /* Points are opposite, return zero */
     return;
   }
   I = Fl_sqr_pre(Fl_double(H, p), p, pi);
@@ -133,9 +109,7 @@ Flj_add_indir_pre(GEN P, GEN Q, GEN R, ulong a4, ulong p, ulong pi)
 
 INLINE void
 Flj_add_pre_inplace(GEN P, GEN Q, ulong a4, ulong p, ulong pi)
-{
-  Flj_add_indir_pre(P, Q, P, a4, p, pi);
-}
+{ Flj_add_indir_pre(P, Q, P, a4, p, pi); }
 
 GEN
 Flj_add_pre(GEN P, GEN Q, ulong a4, ulong p, ulong pi)
@@ -147,9 +121,7 @@ Flj_add_pre(GEN P, GEN Q, ulong a4, ulong p, ulong pi)
 
 GEN
 Flj_neg(GEN Q, ulong p)
-{
-  return mkvecsmall3(Q[1], Fl_neg(Q[2], p), Q[3]);
-}
+{ return mkvecsmall3(Q[1], Fl_neg(Q[2], p), Q[3]); }
 
 typedef struct {
   ulong n; /* The number being represented */
@@ -157,10 +129,8 @@ typedef struct {
   ulong lnzb; /* Leading non-zero bit */
 } naf_t;
 
-/*
- * Return the signed binary representation (i.e. the Non-Adjacent Form
- * in base 2) of 0 <= a < 2^63.
- */
+/* Return the signed binary representation (i.e. the Non-Adjacent Form
+ * in base 2) of 0 <= a < 2^63 */
 static void
 naf_repr(naf_t *x, ulong a)
 {
@@ -184,13 +154,7 @@ naf_repr(naf_t *x, ulong a)
   t = c0 - (c1 << 1);
   /* Note that we don't need to check whether t < 0, since a >= 0 implies
    * that this most significant signed bit must be non-negative. */
-#if 0
-  if (t < 0)
-    nbits |= (1UL << i);
-  else
-#endif
-  if (t > 0)
-    pbits |= (1UL << i);
+  if (t > 0) pbits |= (1UL << i);
 
   x->pbits = pbits;
   x->nbits = nbits;
@@ -201,10 +165,7 @@ naf_repr(naf_t *x, ulong a)
   x->lnzb = expu(pbits) - 2;
 }
 
-/*
- * Standard left-to-right signed double-and-add to compute [n]P.
- */
-
+/* Standard left-to-right signed double-and-add to compute [n]P. */
 static GEN
 Flj_mulu_pre_naf(GEN P, ulong n, ulong a4, ulong p, ulong pi, const naf_t *x)
 {
@@ -212,14 +173,11 @@ Flj_mulu_pre_naf(GEN P, ulong n, ulong a4, ulong p, ulong pi, const naf_t *x)
   ulong pbits, nbits, lnzb;
   ulong m;
 
-  if (n == 0)
-    return mkvecsmall3(1, 1, 0);
-  if (n == 1)
-    return Flv_copy(P);
+  if (n == 0) return mkvecsmall3(1, 1, 0);
+  if (n == 1) return Flv_copy(P);
 
   R = Flj_dbl_pre(P, a4, p, pi);
-  if (n == 2)
-    return R;
+  if (n == 2) return R;
 
   pbits = x->pbits;
   nbits = x->nbits;
@@ -259,22 +217,18 @@ Flj_order_ufact(GEN P, ulong n, GEN F, ulong a4, ulong p, ulong pi)
 
   for (i = 1; i < nfactors; ++i) {
     ulong q, pp = primes[i];
-    long ei = exps[i];
-    long j, k;
+    long j, k, ei = exps[i];
     naf_t x;
     GEN b;
 
-    for (q = pp, j = 1; j < ei; ++j)
-      q *= pp;
+    for (q = pp, j = 1; j < ei; ++j) q *= pp;
     b = Flj_mulu_pre(P, n / q, a4, p, pi);
 
     naf_repr(&x, pp);
     for (j = 0; j < ei && b[3] != 0; ++j)
       b = Flj_mulu_pre_naf(b, pp, a4, p, pi, &x);
-    if (b[3] != 0)
-      return 0;
-    for (k = 0; k < j; ++k)
-      res *= pp;
+    if (b[3] != 0) return 0;
+    for (k = 0; k < j; ++k) res *= pp;
     avma = av;
   }
   return res;
@@ -388,8 +342,7 @@ Fle_add_slope(GEN P, GEN Q, ulong a4, ulong p, ulong *slope)
   if (ell_is_inf(Q)) return P;
   Px = P[1]; Py = P[2];
   Qx = Q[1]; Qy = Q[2];
-  if (Px==Qx)
-    return Py==Qy ? Fle_dbl_slope(P, a4, p, slope): ellinf();
+  if (Px==Qx) return Py==Qy ? Fle_dbl_slope(P, a4, p, slope): ellinf();
   *slope = Fl_div(Fl_sub(Py, Qy, p), Fl_sub(Px, Qx, p), p);
   Rx = Fl_sub(Fl_sub(Fl_sqr(*slope, p), Px, p), Qx, p);
   Ry = Fl_sub(Fl_mul(*slope, Fl_sub(Px, Rx, p), p), Py, p);
@@ -418,11 +371,7 @@ Fle_sub(GEN P, GEN Q, ulong a4, ulong p)
   return gerepileupto(av, Fle_add_slope(P, Fle_neg(Q, p), a4, p, &slope));
 }
 
-struct _Fle
-{
-  ulong a4,a6;
-  ulong p;
-};
+struct _Fle { ulong a4, a6, p; };
 
 static GEN
 _Fle_dbl(void *E, GEN P)
@@ -473,7 +422,6 @@ Fle_mul(GEN P, GEN n, ulong a4, ulong p)
 }
 
 /* Finds a random non-singular point on E */
-
 GEN
 random_Fle_pre(ulong a4, ulong a6, ulong p, ulong pi)
 {
@@ -519,16 +467,15 @@ ulong
 Fl_ellj(ulong a4, ulong a6, ulong p)
 {
   if (SMALL_ULONG(p))
-  {
-    /* a43 = 4 a4^3 */
+  { /* a43 = 4 a4^3 */
     ulong a43 = Fl_double(Fl_double(Fl_mul(a4, Fl_sqr(a4, p), p), p), p);
     /* a62 = 27 a6^2 */
     ulong a62 = Fl_mul(Fl_sqr(a6, p), 27 % p, p);
     ulong z1 = Fl_mul(a43, 1728 % p, p);
     ulong z2 = Fl_add(a43, a62, p);
     return Fl_div(z1, z2, p);
-  } else
-    return Fl_ellj_pre(a4, a6, p, get_Fl_red(p));
+  }
+  return Fl_ellj_pre(a4, a6, p, get_Fl_red(p));
 }
 
 void
@@ -549,8 +496,7 @@ Fl_ellj_to_a4a6(ulong j, ulong p, ulong *pt_a4, ulong *pt_a6)
 
 ulong
 Fl_elldisc_pre(ulong a4, ulong a6, ulong p, ulong pi)
-{
-  /* D = -(4A^3 + 27B^2) */
+{ /* D = -(4A^3 + 27B^2) */
   ulong t1, t2;
   t1 = Fl_mul_pre(a4, Fl_sqr_pre(a4, p, pi), p, pi);
   t1 = Fl_double(Fl_double(t1, p), p);
@@ -562,40 +508,35 @@ ulong
 Fl_elldisc(ulong a4, ulong a6, ulong p)
 {
   if (SMALL_ULONG(p))
-  {
-    /* D = -(4A^3 + 27B^2) */
+  { /* D = -(4A^3 + 27B^2) */
     ulong t1, t2;
     t1 = Fl_mul(a4, Fl_sqr(a4, p), p);
     t1 = Fl_double(Fl_double(t1, p), p);
     t2 = Fl_mul(27 % p, Fl_sqr(a6, p), p);
     return Fl_neg(Fl_add(t1, t2, p), p);
-  } else
-    return Fl_elldisc_pre(a4, a6, p, get_Fl_red(p));
+  }
+  return Fl_elldisc_pre(a4, a6, p, get_Fl_red(p));
 }
 
 static ulong
 nonsquare_Fl(ulong p)
 {
   ulong a;
-  do
-    a = random_Fl(p);
-  while (krouu(a, p) >= 0);
+  do a = random_Fl(p); while (krouu(a, p) >= 0);
   return a;
 }
 
 void
-Fl_elltwist_disc(ulong a4, ulong a6, ulong D, ulong p, ulong *pt_a4, ulong *pt_a6)
+Fl_elltwist_disc(ulong a4, ulong a6, ulong D, ulong p, ulong *pa4, ulong *pa6)
 {
   ulong D2 = Fl_sqr(D, p);
-  *pt_a4 = Fl_mul(a4, D2, p);
-  *pt_a6 = Fl_mul(a6, Fl_mul(D, D2, p), p);
+  *pa4 = Fl_mul(a4, D2, p);
+  *pa6 = Fl_mul(a6, Fl_mul(D, D2, p), p);
 }
 
 void
 Fl_elltwist(ulong a4, ulong a6, ulong p, ulong *pt_a4, ulong *pt_a6)
-{
-  Fl_elltwist_disc(a4, a6, nonsquare_Fl(p), p, pt_a4, pt_a6);
-}
+{ Fl_elltwist_disc(a4, a6, nonsquare_Fl(p), p, pt_a4, pt_a6); }
 
 static void
 Fle_dbl_sinv_pre_inplace(GEN P, ulong a4, ulong sinv, ulong p, ulong pi)
@@ -661,7 +602,7 @@ FleV_add_pre_inplace(GEN P, GEN Q, GEN a4, ulong p, ulong pi)
     uel(sinv,i) = umael(P,i,1)==p? 1: skipzero(Fl_sub(mael(P,i,1), mael(Q,i,1), p));
   Flv_inv_pre_inplace(sinv, p, pi);
   for (i=1; i<l; i++)
-    Fle_add_sinv_pre_inplace(gel(P, i), gel(Q, i), uel(a4, i), uel(sinv, i), p, pi);
+    Fle_add_sinv_pre_inplace(gel(P,i), gel(Q,i), uel(a4,i), uel(sinv,i), p, pi);
 }
 
 void
@@ -673,7 +614,7 @@ FleV_sub_pre_inplace(GEN P, GEN Q, GEN a4, ulong p, ulong pi)
     uel(sinv,i) = umael(P,i,1)==p? 1: skipzero(Fl_sub(mael(P,i,1), mael(Q,i,1), p));
   Flv_inv_pre_inplace(sinv, p, pi);
   for (i=1; i<l; i++)
-    Fle_sub_sinv_pre_inplace(gel(P, i), gel(Q, i), uel(a4, i), uel(sinv, i), p, pi);
+    Fle_sub_sinv_pre_inplace(gel(P,i), gel(Q,i), uel(a4,i), uel(sinv,i), p, pi);
 }
 
 void
@@ -685,16 +626,15 @@ FleV_dbl_pre_inplace(GEN P, GEN a4, ulong p, ulong pi)
     uel(sinv,i) = umael(P,i,1)==p? 1: skipzero(Fl_double(umael(P,i,2), p));
   Flv_inv_pre_inplace(sinv, p, pi);
   for(i=1; i<l; i++)
-    Fle_dbl_sinv_pre_inplace(gel(P, i), uel(a4, i), uel(sinv, i), p, pi);
+    Fle_dbl_sinv_pre_inplace(gel(P,i), uel(a4,i), uel(sinv,i), p, pi);
 }
 
 static void
 FleV_mulu_pre_naf_inplace(GEN P, ulong n, GEN a4, ulong p, ulong pi, const naf_t *x)
 {
   pari_sp av = avma;
+  ulong pbits, nbits, lnzb, m;
   GEN R;
-  ulong pbits, nbits, lnzb;
-  ulong m;
   if (n == 1) return;
 
   R = P; P = gcopy(P);
