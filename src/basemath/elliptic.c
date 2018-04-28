noloader/pari

commit 7f2daa9e7d0e2577ac2a3888f24ea259f735cb6c

simplify ellminimaltwistcond + whitespace edit

src/basemath/elliptic.c
@@ -1633,12 +1633,14 @@ ellminimaltwistcond(GEN e)
   if (f==4) D = negi(v==3 ? D: shifti(D, v==0? 2: -2));
   else if (f==6)
   {
-    if (v < 3) D = shifti(D, v==0? 3: 1);
+    long s, t;
+    if (v < 3) s = v==0? 3: 1;
     else
     {
-      long si = (mod32(D)>>3)==1? 1: -1;
-      D = shifti(D, signe(D)==si ? -3: -1);
+      t = v==3? 1: -1;
+      s = signe(D)==t ? -3: -1;
     }
+    D = shifti(D, s);
   }
   return gerepileuptoleaf(av, D);
 }
@@ -1648,10 +1650,8 @@ ellminimaltwist0(GEN e, long flag)
 {
   switch(flag)
   {
-    case 0:
-      return ellminimaltwist(e);
-    case 1:
-      return ellminimaltwistcond(e);
+    case 0: return ellminimaltwist(e);
+    case 1: return ellminimaltwistcond(e);
   }
   pari_err_FLAG("ellminimaltwist");
   return NULL; /* LCOV_EXCL_LINE */

commit f53ea25d2b51212444249e56d27f95712bebc4ec

ellminimaltwistcond: fix bug introduced in 7f2daa9e
E=ellinit("864e1");ellminimaltwist(E,1) -> 3 [ should be 12 ]

src/basemath/elliptic.c
@@ -1637,7 +1637,7 @@ ellminimaltwistcond(GEN e)
     if (v < 3) s = v==0? 3: 1;
     else
     {
-      t = v==3? 1: -1;
+      t = (v==3 && mod32(D) == 8)? 1: -1;
       s = signe(D)==t ? -3: -1;
     }
     D = shifti(D, s);

src/test/32/ell
@@ -262,6 +262,7 @@ ecsmall([1]), [Vecsmall([128, -1])], [0, 0, 0, 0, 0, 0, 0, 0]]
 [[1, 64], [1, 64], [-8, 64], [-8, 64]]
 [[1, -15], [-4, -15], [8, -15], [-8, -15]]
 [[1, -11648], [1, -11648], [-8, -11648], [-8, -11648]]
+12
 a
 a
 [5, -66, 148, 665, -15802]
@@ -735,4 +736,4 @@ t + 1/3*t^3 - 1/2*t^4 + 14/15*t^5 + O(t^6)
   *** ellformalw: domain error in ellformalw: precision <= 0
 242.47010035195076100129810400142304776
 0
-Total time spent: 659
+Total time spent: 613

src/test/in/ell
@@ -116,6 +116,8 @@ a=4; twisttest(3*2^a, 0, T2)
 twisttest(1, -161, T2)
 d=2; twisttest(-17*2^(2*d), 2^(3*d), T2)
 
+ellminimaltwist(ellinit([216,-432]),1)
+
 a=ffgen(2^5,'a);ellinit(ellfromj(a)).j
 a=ffgen(3^5,'a);ellinit(ellfromj(a)).j
 