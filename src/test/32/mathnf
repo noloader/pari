noloader/pari

commit 8e990e706991af6f4e6e3d71ee32875be263899f

hnflll: remove HNFLLL_QUALITY-protected code
Dead code since 1998: never made any positive difference to enable it, with
alpha = 1/2, 3/4 or 1 (default). Simplify code.

src/basemath/hnf_snf.c
@@ -1659,9 +1659,6 @@ GEN
 ZM_hnflll(GEN A, GEN *ptB, int remove)
 {
   pari_sp av = avma;
-#ifdef HNFLLL_QUALITY
-  const long m1 = 1, n1 = 1; /* alpha = m1/n1. Maybe 3/4 here ? */
-#endif
   long n, k, kmax;
   GEN B, lambda, D;
 
@@ -1682,11 +1679,7 @@ ZM_hnflll(GEN A, GEN *ptB, int remove)
     { /* row0 == row1 == 0 */
       pari_sp av1 = avma;
       GEN z = addii(mulii(gel(D,k-2),gel(D,k)), sqri(gcoeff(lambda,k-1,k)));
-#ifdef HNFLLL_QUALITY
-      do_swap = (cmpii(mului(n1,z), mului(m1,sqri(gel(D,k-1)))) < 0);
-#else /* assume m1 = n1 = 1 */
       do_swap = (cmpii(z, sqri(gel(D,k-1))) < 0);
-#endif
       avma = av1;
     }
     else
@@ -1774,9 +1767,6 @@ reduce1(GEN A, GEN B, long k, long j, GEN lambda, GEN D)
 static GEN
 ZV_gcdext_i(GEN A)
 {
-#ifdef HNFLLL_QUALITY
-  const long m1 = 1, n1 = 1; /* alpha = m1/n1. Maybe 3/4 here ? */
-#endif
   long k, n = lg(A);
   GEN B, lambda, D;
 
@@ -1796,11 +1786,7 @@ ZV_gcdext_i(GEN A)
     {
       pari_sp av1 = avma;
       GEN z = addii(mulii(gel(D,k-2),gel(D,k)), sqri(gcoeff(lambda,k-1,k)));
-#ifdef HNFLLL_QUALITY
-      do_swap = (cmpii(mului(n1,z), mului(m1,sqri(gel(D,k-1)))) < 0);
-#else /* assume m1 = n1 = 1 */
       do_swap = (cmpii(z, sqri(gel(D,k-1))) < 0);
-#endif
       avma = av1;
     }
     else do_swap = 0;
@@ -2019,10 +2005,10 @@ ZM_hnfall_i(GEN A, GEN *ptB, long remove)
     {
       for (i=h[j]; i>li; i--)
       {
-        a = gcoeff(A,i,j);
+        a = gcoeff(A,i,j); if (!signe(a)) continue;
         k = c[i];
         /* zero a = Aij  using  Aik */
-        if (signe(a)) ZC_elem(a,gcoeff(A,i,k), A,B,j,k);
+        ZC_elem(a,gcoeff(A,i,k), A,B,j,k);
         ZM_reduce(A,B, i,k); /* ensure reduced entries */
         if (gc_needed(av,1))
         {

commit bf767894f38d86c035a660adf6009a1071b2f2ad

revert regression introduced in 8e990e70 [#2055]

src/basemath/hnf_snf.c
@@ -1999,11 +1999,11 @@ ZM_hnfall_i(GEN A, GEN *ptB, long remove)
     {
       for (i=h[j]; i>li; i--)
       {
-        a = gcoeff(A,i,j); if (!signe(a)) continue;
+        a = gcoeff(A,i,j);
         k = c[i];
         /* zero a = Aij  using  Aik */
-        ZC_elem(a,gcoeff(A,i,k), A,B,j,k);
-        ZM_reduce(A,B, i,k); /* ensure reduced entries */
+        if (signe(a)) ZC_elem(a,gcoeff(A,i,k), A,B,j,k);
+        ZM_reduce(A,B, i,k); /* ensure reduced entries even if a = 0 */
         if (gc_needed(av,1))
         {
           if (DEBUGMEM>1)

src/test/32/mathnf
@@ -31,6 +31,30 @@
 [[0, 0, 0, 1], 2833319]
 ? [H,U]=mathnf(v,5);[v*U,norml2(U)]
 [[0, 0, 0, 1], 765585180708864230567243002686057927228240493]
+? M=[0,0,0,0,0,0,0,0,0,13;0,0,0,0,0,0,0,0,23,6;0,0,0,0,0,0,0,23,-4,-7;0,0,0,0,0,0,17,-3,5,-5;0,0,0,0,0,56,16,-16,-15,-17;0,0,0,0,57,24,-16,-25,2,-21;0,0,0,114,9,56,51,-52,25,-55;0,0,113,-31,-11,24,0,28,34,-16;0,50,3,2,16,-6,-2,7,-19,-21;118,43,51,23,37,-52,18,38,51,28];mathnf(M)
+
+[787850171872400 32189386376004 356588299060422 742392731867995 282253457851
+430 665185047494955 664535243562463 744564809133574 113975061998590 52745901
+3372200]
+
+[0 12 6 11 5 3 7 6 6 0]
+
+[0 0 3 1 2 1 1 0 0 0]
+
+[0 0 0 1 0 0 0 0 0 0]
+
+[0 0 0 0 1 0 0 0 0 0]
+
+[0 0 0 0 0 1 0 0 0 0]
+
+[0 0 0 0 0 0 1 0 0 0]
+
+[0 0 0 0 0 0 0 1 0 0]
+
+[0 0 0 0 0 0 0 0 1 0]
+
+[0 0 0 0 0 0 0 0 0 1]
+
 ? mathnf([])
 [;]
 ? mathnf([],1)

src/test/in/mathnf
@@ -9,6 +9,8 @@ mathnf([x,x^2+1; x^3+x+1, x+2]*Mod(1,5))
 v=[116085838, 181081878, 314252913,10346840];
 [H,U]=mathnf(v, 1); [v*U, norml2(U)]
 [H,U]=mathnf(v, 5); [v*U, norml2(U)]
+M=[0,0,0,0,0,0,0,0,0,13;0,0,0,0,0,0,0,0,23,6;0,0,0,0,0,0,0,23,-4,-7;0,0,0,0,0,0,17,-3,5,-5;0,0,0,0,0,56,16,-16,-15,-17;0,0,0,0,57,24,-16,-25,2,-21;0,0,0,114,9,56,51,-52,25,-55;0,0,113,-31,-11,24,0,28,34,-16;0,50,3,2,16,-6,-2,7,-19,-21;118,43,51,23,37,-52,18,38,51,28]; mathnf(M)
+
 mathnf([])
 mathnf([],1)
 mathnf([;])