deddfb1c4f59f205166a5561ea09276c52cab1a0
Karim Belabas <Karim.Belabas@math.u-bordeaux.fr>
2019-08-09 10:27:10 +0200

    rewrite develop.tex's Chapter 3 and include the pari-mt.c example

--- examples/pari-mt.c
+++ examples/pari-mt.c
@@ -1,51 +1,33 @@
-#include <pari/pari.h> /* Include PARI headers */
+#include <pari/pari.h>
 
 GEN
-my_worker(GEN d, long l)
-{
-  return l==1 ? Z_factor(d) : det(d);
-}
+Cworker(GEN d, long kind) { return kind? det(d): Z_factor(d); }
 
 int
 main(void)
 {
-  long i;
-  GEN M,N1,N2, F1,F2,D;
-  GEN input, output;
+  long i, taskid, pending;
+  GEN M,N1,N2, F1,F2,D, in,out, done;
   struct pari_mt pt;
-  GEN done, worker;
-  long workid, pending;
-  entree ep_worker={"_worker",0,(void*)my_worker,14,"GL",""};
-  /* Initialize the main PARI stack and global objects (gen_0, etc.)
-     Postpone initialization of parallelism */
-  pari_init_opts(8000000,500000,INIT_JMPm | INIT_SIGm | INIT_DFTm | INIT_noIMTm);
-  /* Add my_worker function to gp */
-  pari_add_function(&ep_worker);
-  /* Initialize parallelism, now that my_worker is registered */
-  pari_mt_init();
-  /* Compute in the main PARI stack */
+  entree ep = {"_worker",0,(void*)Cworker,20,"GL",""};
+  /* initialize PARI, postponing parallelism initialization */
+  pari_init_opts(8000000,500000, INIT_JMPm|INIT_SIGm|INIT_DFTm|INIT_noIMTm);
+  pari_add_function(&ep); /* add Cworker function to gp */
+  pari_mt_init(); /* ... THEN initialize parallelism */
+  /* Create inputs and room for output in main PARI stack */
   N1 = addis(int2n(256), 1); /* 2^256 + 1 */
   N2 = subis(int2n(193), 1); /* 2^193 - 1 */
   M = mathilbert(80);
-  /* Create input and output vectors */
-  input  = mkvec3(N1,N2,M);
-  output = cgetg(4,t_VEC);
-  /* Initialize parallel evaluation of my_worker */
-  worker = strtofunction("_worker");
-  mt_queue_start(&pt, worker);
-  for (i=1; i<=3 || pending; i++)
-  {
-    /* submit job (input) */
-    mt_queue_submit(&pt, i,
-      i<=3? mkvec2(gel(input,i),i<=2 ? gen_1: gen_2): NULL);
-    /* get result (output) */
-    done = mt_queue_get(&pt, &workid, &pending);
-    if (done) gel(output,workid) = done;
+  in  = mkvec3(mkvec2(N1,gen_1), mkvec2(N2,gen_1), mkvec2(M,gen_0));
+  out = cgetg(4,t_VEC);
+  /* Initialize parallel evaluation of Cworker */
+  mt_queue_start(&pt, strtofunction("_worker"));
+  for (i = 1; i <= 3 || pending; i++)
+  { /* submit job (in) and get result (out) */
+    mt_queue_submit(&pt, i, i<=3? gel(in,i): NULL);
+    done = mt_queue_get(&pt, &taskid, &pending);
+    if (done) gel(out,taskid) = done;
   }
-  /* end parallelism */
-  mt_queue_end(&pt);
-  F1 = gel(output,1); F2 = gel(output,2); D = gel(output,3);
-  pari_printf("F1=%Ps\nF2=%Ps\nlog(D)=%Ps\n", F1, F2, glog(D,3));
-  pari_close();
-  return 0;
+  mt_queue_end(&pt); /* end parallelism */
+  output(out); pari_close(); return 0;
 }
