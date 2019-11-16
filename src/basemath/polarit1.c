824e9ab687fc2f28ce2c866f47c08aa90900eb86
Karim Belabas <Karim.Belabas@math.u-bordeaux.fr>
2019-11-16 20:50:10 +0100

    factorpadic: test with is_pm1 instead of == gen_1

--- src/basemath/polarit1.c
+++ src/basemath/polarit1.c
@@ -581,8 +581,8 @@ factorpadic(GEN f, GEN p, long r)
     f = pnormalize(f, p, NULL, r, n-1, &lead, &pr, &reverse);
     y = ZpX_monic_factor(f, p, pr);
     P = gel(y,1); l = lg(P);
-    if (lead != gen_1)
-      for (i=1; i<l; i++) gel(P,i) = Q_primpart( RgX_unscale(gel(P,i), lead) );
+    if (is_pm1(lead))
+      for (i=1; i<l; i++) gel(P,i) = Q_primpart(RgX_unscale(gel(P,i), lead));
     for (i=1; i<l; i++)
     {
       GEN t = gel(P,i);
