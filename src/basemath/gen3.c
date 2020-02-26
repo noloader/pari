049f395cf453e740cf49b84aabac68a84cfb559b
Karim Belabas <Karim.Belabas@math.u-bordeaux.fr>
2020-02-26 01:11:30 +0100

    GC mistake in subst
    subst(((-6*t^2-t)*u^4+(t^2)*u^3+(P)*t^3*u)/u^2,t,0) -> type error

--- src/basemath/gen3.c
+++ src/basemath/gen3.c
@@ -1476,7 +1476,7 @@ gsubst(GEN x, long v, GEN y)
         if (lx == 2) return z;
         for (i = 2; i < lx; i++) gel(z,i) = gsubst(gel(x,i),v,y);
         z = normalizepol_lg(z, lx); lx = lg(z);
-        if (lx == 2) return gc_const(av, zeropol(vx));
+        if (lx == 2) { set_avma(av); return zeropol(vx); }
         if (lx == 3) return gerepileupto(av, gmul(pol_1(vx), gel(z,2)));
         return gerepileupto(av, poleval(z, pol_x(vx)));
       }
