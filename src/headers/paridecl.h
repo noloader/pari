8936ee7bae2260300467f1a9cb5f1e4f90d0ebf4
Bill Allombert <Bill.Allombert@math.u-bordeaux.fr>
2020-04-11 13:09:00 +0200

    paridecl.h: fix typo F2m_to_F2Ms_ker -> F2m_to_F2Ms

--- src/headers/paridecl.h
+++ src/headers/paridecl.h
@@ -98,7 +98,7 @@ GEN     F2m_powu(GEN x, ulong n);
 long    F2m_rank(GEN x);
 GEN     F2m_row(GEN x, long j);
 GEN     F2m_rowslice(GEN x, long a, long b);
-GEN     F2m_to_F2Ms_ker(GEN M);
+GEN     F2m_to_F2Ms(GEN M);
 GEN     F2m_to_Flm(GEN z);
 GEN     F2m_to_ZM(GEN z);
 GEN     F2m_to_mod(GEN z);
